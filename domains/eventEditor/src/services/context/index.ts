d5d9f13ff89b50ab7d462a3b3999d7a6c0892539
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-06-19 21:52:28 +0530

    Just do it

---
+++ domains/eventEditor/src/services/context/index.ts
@@ -0,0 +1,7 @@
+export * from './EntityListContext';
+
+export * from './EventContext';
+
+export * from './EventEditorContext';
+
+export * from './EdtrStateContext';
---
+++ domains/eventEditor/src/ui/tickets/ticketsList/actionsMenu/dropdown/useActions.ts
@@ -0,0 +1,32 @@
+import { useCallback, useMemo } from 'react';
+import { __ } from '@wordpress/i18n';
+
+import { isTrashed } from '@eventespresso/predicates';
+import { useTicketItem } from '@eventespresso/edtr-services';
+import useDeleteTicketHandler from '@edtrUI/tickets/hooks/useDeleteTicketHandler';
+import useCopyTicket from './useCopyTicket';
+
+const useActions = ({ ticketId }) => {
+	// Make sure to subscribe to Apollo cache
+	// to avoid stale data
+	const ticket = useTicketItem({ id: ticketId });
+
+	const deleteTicket = useDeleteTicketHandler(ticketId);
+
+	const trashed = isTrashed(ticket);
+
+	const trashTicket = useCallback(() => deleteTicket(trashed), [deleteTicket, trashed]);
+
+	const copyTicket = useCopyTicket(ticket);
+
+	return useMemo(
+		() => ({
+			copyTicket,
+			trashTicket,
+			trashed,
+		}),
+		[copyTicket, trashTicket, trashed]
+	);
+};
+
+export default useActions;
