94c7ab189e0e4e62cc8abf2a441872dfdb25f118
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-06-29 12:09:03 +0300

    Small cleanup.

--- domains/rem/src/components/MultiStep/types.ts
+++ domains/rem/src/components/MultiStep/types.ts
@@ -1,16 +1,12 @@
 import type { Disclosure } from '@eventespresso/services';
-import type { EntityId } from '@eventespresso/data';
 import type { Datetime } from '@eventespresso/edtr-services';
-import type { FormRenderProps } from 'react-final-form';
-import type { DateFormShape } from '../types';
+// import type { FormRenderProps } from 'react-final-form';
 
-export interface ContainerProps extends Omit<Disclosure, 'onOpen'> {
-	datetimeId?: EntityId;
-}
+export interface ContainerProps extends Omit<Disclosure, 'onOpen'> {}
 
 export interface ContentProps {
 	entity: Datetime;
 	onClose: VoidFunction;
 }
 
-export interface ContentWrapperProps extends FormRenderProps<DateFormShape> {}
+// export interface ContentWrapperProps extends FormRenderProps<> {}
