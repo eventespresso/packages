dd091a7473acc12ac91d2cfbba7845048981bbb8
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-07-16 15:27:21 +0300

    Add ticket button and ticket offsets.

--- domains/rem/src/ui/Tickets/TicketCard.tsx
+++ domains/rem/src/ui/Tickets/TicketCard.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { differenceInHours, isAfter, parseISO } from 'date-fns';
+import { __ } from '@wordpress/i18n';
 
 import { CurrencyInput } from '@eventespresso/components';
 import { SimpleEntityCard } from '@eventespresso/components';
 import { TicketCardProps } from './types';
+import { useDatetime } from '../../context';
 
 const TicketCard: React.FC<TicketCardProps> = ({ ticket }) => {
 	const beforeDetails = <CurrencyInput id={ticket.id} amount={ticket.price} tag='h5' vertical />;
+	const datetime = useDatetime();
 
-	return <SimpleEntityCard beforeDetails={beforeDetails} id={ticket.id} name={ticket.name} />;
+	const datetimeStartDate = parseISO(datetime.startDate);
+	const ticketStartDate = parseISO(ticket.startDate);
+
+	const isTicketAfter = isAfter(ticketStartDate, datetimeStartDate);
+	const ticketStartDifferenceInHours = differenceInHours(ticketStartDate, datetimeStartDate);
+
+	const afterDetails = (
+		<div className='ee-ticket-offset'>
+			<div>
+				{`${__('starts:')} ${ticketStartDifferenceInHours} hours ${
+					isTicketAfter ? __('after') : __('before')
+				} start date`}
+			</div>
+		</div>
+	);
+
+	return (
+		<SimpleEntityCard afterDetails={afterDetails} beforeDetails={beforeDetails} id={ticket.id} name={ticket.name} />
+	);
 };
 
 export default React.memo(TicketCard);
--- domains/rem/src/ui/Tickets/index.tsx
+++ domains/rem/src/ui/Tickets/index.tsx
@@ -2,20 +2,25 @@ import React, { Fragment } from 'react';
 
 import { useTickets } from '@eventespresso/edtr-services';
 
+import AddTicket from './AddTicket';
 import TicketCard from './TicketCard';
+
 import './style.scss';
 
 const Tickets: React.FC = () => {
 	const tickets = useTickets();
 
 	return (
-		<div className='rem-ticket-list'>
-			{tickets.map((ticket) => (
-				<Fragment key={ticket.id}>
-					<TicketCard ticket={ticket} />
-				</Fragment>
-			))}
-		</div>
+		<>
+			<div className='rem-ticket-list'>
+				{tickets.map((ticket) => (
+					<Fragment key={ticket.id}>
+						<TicketCard ticket={ticket} />
+					</Fragment>
+				))}
+				<AddTicket />
+			</div>
+		</>
 	);
 };
 
