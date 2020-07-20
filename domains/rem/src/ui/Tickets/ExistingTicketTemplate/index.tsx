76cb6eecfbdb271c2d01863efc94d251bfab5b90
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-07-20 23:04:57 +0300

    Added ExistingTicketTemplate.

---
+++ domains/rem/src/ui/Tickets/ExistingTicketTemplate/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useCallback } from 'react';
+import { __ } from '@wordpress/i18n';
+
+import { Ticket, useTickets } from '@eventespresso/edtr-services';
+import { Button, SelectInput } from '@eventespresso/components';
+import { getGuids } from '@eventespresso/predicates';
+import { entityListToSelectOptions } from '@eventespresso/services';
+
+import './style.scss';
+
+interface Props {
+	ticketTemplates: Ticket[];
+	addTicketTemplate: (ticket: Ticket) => void;
+}
+
+const ExistingTicketTemplate: React.FC<Props> = ({ addTicketTemplate, ticketTemplates }) => {
+	const [selectedTicketId, setSelectedTicketId] = useState('');
+
+	const tickets = useTickets();
+
+	const filteredTickets = tickets.filter(({ id }) => !getGuids(ticketTemplates).includes(id));
+
+	const onChangeValue = useCallback((value) => setSelectedTicketId(value), []);
+
+	const options = entityListToSelectOptions(filteredTickets, { label: __('Select...'), value: '' });
+
+	const ticket = tickets.find(({ id }) => id === selectedTicketId);
+
+	const onClick = useCallback(() => addTicketTemplate(ticket), [ticket, addTicketTemplate]);
+
+	return (
+		<div className='rem-add-existing-ticket'>
+			<SelectInput options={options} onChangeValue={onChangeValue} />
+			<Button
+				buttonText={__('Use Ticket as Template')}
+				onClick={onClick}
+				isDisabled={!selectedTicketId || !filteredTickets?.length}
+			/>
+		</div>
+	);
+};
+
+export default ExistingTicketTemplate;
