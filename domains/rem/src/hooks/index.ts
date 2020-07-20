cb632c6e3a72e628c5296b6f444cefda581ac11c
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-07-20 21:51:31 +0530

    Remove Apollo and date provider

--- domains/rem/src/hooks/index.ts
+++ domains/rem/src/hooks/index.ts
@@ -1,7 +1,1 @@
-export { default as useEditorInit } from './useEditorInit';
-
-export { default as useRemInitialization } from './useRemInitialization';
-
 export * from './newEntityOptions';
-
-export * from './entityListCacheUpdates';
