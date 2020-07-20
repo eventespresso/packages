5a57d03193cc627a64ed80dd01e14414a5a84c58
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-07-20 21:06:36 +0530

    Remove useSubscriptionService

--- packages/registry/src/subscription/index.ts
+++ packages/registry/src/subscription/index.ts
@@ -2,7 +2,5 @@ export { default as SubscriptionManager } from './SubscriptionManager';
 
 export { default as UIRegistry } from './UIRegistry';
 
-export { default as useSubscriptionService } from './useSubscriptionService';
-
 export * from './types';
 export * from './utils';
