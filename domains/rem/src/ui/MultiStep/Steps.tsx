48db8cebaf6c67119db71368c1563e1045dd7b41
Vadim Nicolai <vadim@eventespresso.com>
2020-07-22 19:03:18 +0300

    Update domains/rem/src/ui/MultiStep/Steps.tsx
    Co-authored-by: Brent Christensen <brent@eventespresso.com>

--- domains/rem/src/ui/MultiStep/Steps.tsx
+++ domains/rem/src/ui/MultiStep/Steps.tsx
@@ -8,7 +8,7 @@ import { Calendar, Ticket, CalendarOutlined } from '@eventespresso/icons';
 const Steps: React.FC<Pick<PrevNext, 'current'>> = ({ current }) => {
 	return (
 		<StepsAdapter compact current={current} showStepNumber>
-			<Step icon={Calendar} title={__('Pattern editor')} />
+			<Step icon={Calendar} title={__('Pattern Editor')} />
 			<Step description={__('primary information about the date')} icon={Calendar} title={__('Date Details')} />
 			<Step icon={Ticket} title={__('Tickets')} />
 			<Step icon={CalendarOutlined} title={__('Generated Dates')} />
