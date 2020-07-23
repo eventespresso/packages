684647a8e3eb2ad422955ffe7cbbac9fd8893b97
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-07-23 15:56:41 +0530

    Update ContentFooter.tsx

--- domains/rem/src/ui/MultiStep/ContentFooter.tsx
+++ domains/rem/src/ui/MultiStep/ContentFooter.tsx
@@ -41,7 +41,7 @@ const ContentFooter: React.FC<Props> = ({ onClose }) => {
 				// hide next on last step
 				current < 3 && <Next onClick={next} isDisabled={isNextDisabled} />
 			}
-			<Button mr={3} {...cancelButtonProps} />
+			<Button {...cancelButtonProps} />
 		</ButtonRow>
 	);
 };
