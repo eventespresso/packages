6bede05f7ebb89daec19a2ed9b9165003bfffbf5
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-08-03 19:24:56 +0530

    Fix i18n for sale date specifiers

--- domains/rem/src/ui/Tickets/card/TicketCard.tsx
+++ domains/rem/src/ui/Tickets/card/TicketCard.tsx
@@ -5,6 +5,7 @@ import { CurrencyInput, SimpleEntityCard } from '@eventespresso/components';
 import { BaseProps } from '../types';
 import Sidebar from './Sidebar';
 import { useFormState } from '../../../data';
+import { INTERVALS } from 'domains/rem/src/constants';
 
 const TicketCard: React.FC<BaseProps> = ({ ticket }) => {
 	const { tickets } = useFormState();
@@ -16,20 +17,36 @@ const TicketCard: React.FC<BaseProps> = ({ ticket }) => {
 		<div className='ee-ticket-offset'>
 			<div>
 				{sprintf(
-					__('starts: %1$d %2$s %3$s the %4$s date'),
+					/* translators:
+					1. interval value, like 10 in "10 days", 15 in "15 minutes"
+					2. the interval e.g. "days", "weeks"
+					3. position (before/after) with respect to start or end date
+					4. the date ("start" or "end") for which the position is sepcified
+					The final string may look like this:
+					"starts 3 days before the start date"
+					*/
+					__('starts %1$d %2$s %3$s the %4$s date'),
 					ticketSalesStart?.unitValue,
-					ticketSalesStart?.unit,
-					ticketSalesStart?.position,
-					ticketSalesStart?.startOrEnd
+					INTERVALS?.[ticketSalesStart?.unit],
+					ticketSalesStart?.position === 'before' ? __('before') : __('after'),
+					ticketSalesStart?.startOrEnd === 'start' ? __('start') : __('end')
 				)}
 			</div>
 			<div>
 				{sprintf(
-					__('ends: %1$d %2$s %3$s the %4$s date'),
+					/* translators:
+					1. interval value, like 10 in "10 days", 15 in "15 minutes"
+					2. the interval e.g. "days", "weeks"
+					3. position (before/after) with respect to start or end date
+					4. the date ("start" or "end") for which the position is sepcified
+					The final string may look like this:
+					"ends 3 days before the start date"
+					*/
+					__('ends %1$d %2$s %3$s the %4$s date'),
 					ticketSalesEnd?.unitValue,
-					ticketSalesEnd?.unit,
-					ticketSalesEnd?.position,
-					ticketSalesEnd?.startOrEnd
+					INTERVALS?.[ticketSalesEnd?.unit],
+					ticketSalesEnd?.position === 'before' ? __('before') : __('after'),
+					ticketSalesEnd?.startOrEnd === 'start' ? __('start') : __('end')
 				)}
 			</div>
 		</div>
