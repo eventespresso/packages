09e0ca690515f5d192712405399ab7b1cb6a8d64
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-08-03 14:45:13 +0300

    Adjusted start and end dates for simple card.

--- domains/rem/src/ui/Tickets/card/TicketCard.tsx
+++ domains/rem/src/ui/Tickets/card/TicketCard.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
+import { __, sprintf } from '@wordpress/i18n';
 
 import { CurrencyInput, SimpleEntityCard } from '@eventespresso/components';
 import { BaseProps } from '../types';
 import Sidebar from './Sidebar';
-
-// import { useDatetime } from '../../context';
+import { useFormState } from '../../../data';
 
 const TicketCard: React.FC<BaseProps> = ({ ticket }) => {
-	const beforeDetails = <CurrencyInput id={ticket.id} amount={ticket.price} tag='h5' vertical />;
-	// const datetime = useDatetime();
+	const { tickets } = useFormState();
+	const { ticketSalesStart, ticketSalesEnd } = tickets?.[ticket.id];
 
-	const afterDetails = (
+	const beforeDetails = <CurrencyInput id={ticket.id} amount={ticket.price} tag='h5' vertical />;
+
+	const afterDetails = ticketSalesStart && ticketSalesEnd && (
 		<div className='ee-ticket-offset'>
-			{/* <div>{sprintf(__('starts: %1$d %2$s before the start date'), startDuration, startUnit)}</div> */}
-			{/* <div>{sprintf(__('ends: %1$d %2$s before the end date'), endDuration, endUnit)}</div> */}
+			<div>
+				{sprintf(
+					__('starts: %1$d %2$s %3$s the %4$s date'),
+					ticketSalesStart?.unitValue,
+					ticketSalesStart?.unit,
+					ticketSalesStart?.position,
+					ticketSalesStart?.startOrEnd
+				)}
+			</div>
+			<div>
+				{sprintf(
+					__('ends: %1$d %2$s %3$s the %4$s date'),
+					ticketSalesEnd?.unitValue,
+					ticketSalesEnd?.unit,
+					ticketSalesEnd?.position,
+					ticketSalesEnd?.startOrEnd
+				)}
+			</div>
 		</div>
 	);
 
