0b83920d2651b4c81d7ae59f2d0b846cfd131439
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-08-05 18:24:21 +0530

    Improve group field in form package

--- packages/form/src/fields/Group.tsx
+++ packages/form/src/fields/Group.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import type { FieldProps } from '../types';
 import Field from './Field';
 import FieldRenderer from '../renderers/FieldRenderer';
 import useShouldBeVisible from '../hooks/useShouldBeVisible';
 
-const Group: React.FC<FieldProps> = ({ subFields, label, name: groupName, before, after, conditions }) => {
+const Group: React.FC<FieldProps> = ({
+	subFields,
+	label,
+	name: groupName,
+	before,
+	after,
+	conditions,
+	formControlProps,
+}) => {
 	const visible = useShouldBeVisible(conditions, groupName);
 	if (subFields.length && visible) {
+		const className = classNames('field-group-items', formControlProps?.className);
 		return (
 			<div className='field-group-wrapper'>
 				<h5>{label}</h5>
 				{before}
-				<div className='field-group-items'>
+				<div className={className}>
 					{subFields.map(({ name: fieldname, fieldType, ...props }, i) => {
 						const name = `${groupName}.${fieldname}`;
 						return (
