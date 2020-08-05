1d59a241205a798411f3a114127679b556194aca
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-08-05 23:27:04 +0530

    Update RRuleEditor.tsx

--- domains/rem/src/ui/rRule/RRuleEditor.tsx
+++ domains/rem/src/ui/rRule/RRuleEditor.tsx
@@ -13,7 +13,7 @@ const RRuleEditor: React.FC<RRuleEditorProps> = ({ id, onChange, rRuleString, si
 				{sidebarLabel && <h2 className='rrule-generator__sidebar-label'>{sidebarLabel}</h2>}
 				<PatternEditor id={id} onChange={onChange} rRuleString={rRuleString} type={type} />
 			</div>
-			<DebugInfo data={rRuleString} />
+			<DebugInfo data={rRuleString} asJson={false} />
 		</>
 	);
 };
