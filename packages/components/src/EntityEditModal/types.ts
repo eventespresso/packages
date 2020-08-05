fe944f3f207e79d0fad27d1ff30e34bba48f2c76
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-08-05 20:43:54 +0530

    Improve EndityEditModal component

--- packages/components/src/EntityEditModal/types.ts
+++ packages/components/src/EntityEditModal/types.ts
@@ -1,15 +1,12 @@
 import type { Entity } from '@eventespresso/data';
 import type { Disclosure } from '@eventespresso/services';
+import type { ModalProps } from '@eventespresso/adapters';
 
 export interface BaseProps<E extends Entity> {
 	entity: E;
 }
 
-export interface EntityEditModalProps extends Omit<Disclosure, 'onOpen'> {
-	isOpen: boolean;
-	onClose: VoidFunction;
-	title?: string;
-}
+export interface EntityEditModalProps extends ModalProps {}
 
 export interface ContainerProps<E extends Entity> extends BaseProps<E>, EntityEditModalProps {
 	component: React.ComponentType<BaseProps<E> & Pick<Disclosure, 'onClose'>>;
