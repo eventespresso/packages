919be10e3c1660e3a23eca13138372e0cc6e9f96
Vadim Nicolai <vadim@eventespresso.com>
2020-08-07 18:07:00 +0300

    EditDateRangeButton usage in date and ticket card sidebars. (#158)
    * EditDateRangeButton update.
    
    * EditDateRangeButton usage.

---
+++ packages/adapters/src/Popover/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+	Popover as ChakraPopover,
+	PopoverArrow,
+	PopoverBody,
+	PopoverCloseButton,
+	PopoverContent,
+	PopoverHeader,
+	PopoverProps as ChakraPopoverProps,
+	PopoverTrigger,
+} from '@chakra-ui/popover';
+
+export interface PopoverProps extends ChakraPopoverProps {
+	className?: string;
+	content: React.ReactNode;
+	contentClassName?: string;
+	header?: React.ReactNode;
+	trigger: React.ReactNode;
+}
+
+export const Popover: React.FC<PopoverProps> = React.memo(
+	({ content, contentClassName, header, trigger, ...props }) => {
+		return (
+			<ChakraPopover {...props}>
+				<PopoverTrigger>{trigger}</PopoverTrigger>
+				<PopoverContent zIndex={4} className={contentClassName}>
+					<PopoverArrow />
+					<PopoverCloseButton className='ee-popover__close-button' color='var(--ee-button-text-color)' />
+					{header && (
+						<PopoverHeader className='ee-popover__header' color='var(--ee-button-text-color)'>
+							{header}
+						</PopoverHeader>
+					)}
+					<PopoverBody>{content}</PopoverBody>
+				</PopoverContent>
+			</ChakraPopover>
+		);
+	}
+);
