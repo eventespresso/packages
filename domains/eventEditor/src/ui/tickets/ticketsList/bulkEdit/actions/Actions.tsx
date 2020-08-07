f1d4ea4c77e2f219815987ce3adb59029ede2da1
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-08-07 23:54:02 +0530

    Bulk delete for Dates and Tickets (#159)
    * Enhance confirm dialog
    
    * Create bulk delete mutations for dates
    
    * Wire up bulk delete UI for dates
    
    * Fix memory leak in Inline Edit
    
    * Create bulk delete mutations for tickets
    
    * Wire up bulk delete UI for tickets
    
    * Fix typos

--- domains/eventEditor/src/ui/tickets/ticketsList/bulkEdit/actions/Actions.tsx
+++ domains/eventEditor/src/ui/tickets/ticketsList/bulkEdit/actions/Actions.tsx
@@ -3,14 +3,19 @@ import { __ } from '@wordpress/i18n';
 import { useDisclosure } from '@chakra-ui/hooks';
 
 import { BulkActions, BulkActionsProps } from '@eventespresso/components';
-
-import { EditDetails } from '../details';
 import { useMemoStringify } from '@eventespresso/hooks';
+
+import { useTicketsListFilterState, TicketsStatus } from '@edtrServices/filterState';
+import { EditDetails } from '../details';
+import { Delete } from '../delete';
 
 const Actions: React.FC = () => {
 	const [action, setAction] = useState('');
 
 	const { isOpen, onOpen, onClose } = useDisclosure();
+	const { status } = useTicketsListFilterState();
+
+	const areTrashedTickets = status === TicketsStatus.trashedOnly;
 
 	const options = useMemoStringify([
 		{
@@ -23,7 +28,7 @@ const Actions: React.FC = () => {
 		},
 		{
 			value: 'delete',
-			label: __('delete tickets'),
+			label: areTrashedTickets ? __('delete tickets') : __('trash tickets'),
 		},
 	]);
 
@@ -38,7 +43,12 @@ const Actions: React.FC = () => {
 	return (
 		<>
 			<BulkActions options={options} onApply={onApply} defaultAction='' />
-			{isOpen && <>{action === 'edit-details' && <EditDetails isOpen={true} onClose={onClose} />}</>}
+			{isOpen && (
+				<>
+					{action === 'edit-details' && <EditDetails isOpen={true} onClose={onClose} />}
+					{action === 'delete' && <Delete areTrashedTickets={areTrashedTickets} onClose={onClose} />}
+				</>
+			)}
 		</>
 	);
 };
