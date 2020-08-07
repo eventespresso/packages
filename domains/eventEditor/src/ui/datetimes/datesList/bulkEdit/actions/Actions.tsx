488583ede0fe661bf6ca22eb6e944b50fea44e3a
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-08-07 22:01:52 +0530

    Wire up bulk delete UI for dates

--- domains/eventEditor/src/ui/datetimes/datesList/bulkEdit/actions/Actions.tsx
+++ domains/eventEditor/src/ui/datetimes/datesList/bulkEdit/actions/Actions.tsx
@@ -5,12 +5,18 @@ import { useDisclosure } from '@chakra-ui/hooks';
 import { BulkActions, BulkActionsProps } from '@eventespresso/components';
 
 import { EditDetails } from '../details';
+import { Delete } from '../delete';
 import { useMemoStringify } from '@eventespresso/hooks';
+import { useDatesListFilterState, DatetimeStatus } from '@edtrServices/filterState';
 
 const Actions: React.FC = () => {
 	const [action, setAction] = useState('');
 
 	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	const { status } = useDatesListFilterState();
+
+	const areTrashedDates = status === DatetimeStatus.trashedOnly;
 
 	const options = useMemoStringify([
 		{
@@ -23,7 +29,7 @@ const Actions: React.FC = () => {
 		},
 		{
 			value: 'delete',
-			label: __('delete datetimes'),
+			label: areTrashedDates ? __('delete datetimes') : __('trash datetimes'),
 		},
 	]);
 
@@ -38,7 +44,12 @@ const Actions: React.FC = () => {
 	return (
 		<>
 			<BulkActions options={options} onApply={onApply} defaultAction='' />
-			{isOpen && <>{action === 'edit-details' && <EditDetails isOpen={true} onClose={onClose} />}</>}
+			{isOpen && (
+				<>
+					{action === 'edit-details' && <EditDetails isOpen={true} onClose={onClose} />}
+					{action === 'delete' && <Delete areTrashedDates={areTrashedDates} onClose={onClose} />}
+				</>
+			)}
 		</>
 	);
 };
