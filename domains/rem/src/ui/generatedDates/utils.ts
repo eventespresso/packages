1b81a74de000c64ecd473e9a5c8984c6f3dd3fa3
Brent Christensen <brent@eventespresso.com>
2020-08-11 17:11:40 -0700

    update legend and styles

--- domains/rem/src/ui/generatedDates/utils.ts
+++ domains/rem/src/ui/generatedDates/utils.ts
@@ -1,3 +1,5 @@
+import { __ } from '@wordpress/i18n';
+
 import { LOCALIZED_DATE_FULL_FORMAT, TIME_ONLY_12H_SHORT_FORMAT } from '@eventespresso/constants';
 import type { TimeZoneTime } from '@eventespresso/services';
 import type { DateType } from './types';
@@ -7,5 +9,17 @@ export const formatDate = (date: Date, format: TimeZoneTime['formatForSite']): s
 };
 
 export const getBgClassName = (type: DateType): string => {
-	return `ee-generated-dates-bg--${type}`;
+	return `ee-generated-date--${type}`;
+};
+
+export const iconClassMap: { [key in DateType]: string } = {
+	gDate: 'ee-generated-date--trash',
+	rDate: 'ee-generated-date--remove',
+	exDate: 'ee-generated-date--add',
+};
+
+export const tooltipMap: { [key in DateType]: string } = {
+	gDate: __('remove from list\nof generated dates'),
+	rDate: __('undo addition of extra date and\nremove from list of generated dates'),
+	exDate: __('undo exclusion and add back\ninto list of generated dates'),
 };
