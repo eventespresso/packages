651b2d51e3dc80439ce8d31d219db666fe9aaee2
Brent Christensen <brent@eventespresso.com>
2020-08-12 10:47:49 -0700

    add translators comments

--- domains/rem/src/ui/EntityOptionsRow/index.tsx
+++ domains/rem/src/ui/EntityOptionsRow/index.tsx
@@ -25,14 +25,20 @@ const EntityOptionsRow: React.FC<EntityOptionsRowProps> = ({
 			<div className='ee-entity-option__options'>
 				<div className='ee-entity-option__option'>
 					<label className={'ee-focus-priority-5'} htmlFor={addNewID}>
-						{sprintf(__('select an existing %s to use as a template.'), type)}
+						{
+							/* translators: select an existing "date / ticket" to use as a template */
+							sprintf(__('select an existing %s to use as a template.'), type)
+						}
 					</label>
 					<div className='ee-entity-option__input'>{selectExisting}</div>
 				</div>
 				<div className='ee-entity-option__separator'>{__('or')}</div>
 				<div className='ee-entity-option__option'>
 					<label className={'ee-focus-priority-5'} htmlFor={selectExistingID}>
-						{sprintf(__('Add new %s and insert details manually'), type)}
+						{
+							/* translators: Add new "date / ticket" and insert details manually */
+							sprintf(__('Add new %s and insert details manually'), type)
+						}
 					</label>
 					<div className='ee-entity-option__input'>{addNew}</div>
 				</div>
