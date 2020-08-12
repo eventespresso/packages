b22f508607452c80bfe4c05798f7503cacae6302
Brent Christensen <brent@eventespresso.com>
2020-08-12 09:57:14 -0700

    import and use date constant

--- packages/rrule-generator/src/components/RRuleGenerator/RRuleText.tsx
+++ packages/rrule-generator/src/components/RRuleGenerator/RRuleText.tsx
@@ -3,13 +3,20 @@ import { RRule } from 'rrule';
 import format from 'date-fns/format';
 import { __, sprintf } from '@wordpress/i18n';
 
+import { LOCALIZED_DATE_FULL_FORMAT } from '@eventespresso/constants';
+
 export interface RRuleTextProps {
 	rRuleString?: string;
 }
 
 const RRuleText: React.FC<RRuleTextProps> = ({ rRuleString }) => {
 	const rRule = RRule.fromString(rRuleString);
-	const ruleText = sprintf(__('%s,%sstarting %s'), rRule.toText(), '\n', format(rRule.options.dtstart, 'PPPP'));
+	const ruleText = sprintf(
+		__('%s,%sstarting %s'),
+		rRule.toText(),
+		'\n',
+		format(rRule.options.dtstart, LOCALIZED_DATE_FULL_FORMAT)
+	);
 
 	return (
 		ruleText && (
