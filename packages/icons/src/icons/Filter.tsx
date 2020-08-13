085345716ee06383a23cb04c8daaae4350501d0f
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-08-13 14:07:23 +0300

    Copied over changes from barista/pull/163.

--- packages/icons/src/icons/Filter.tsx
+++ packages/icons/src/icons/Filter.tsx
@@ -4,15 +4,7 @@ import { IconProps } from '../types';
 
 const SvgFilter = (props: IconProps): JSX.Element => {
 	return (
-		<svg
-			aria-hidden='true'
-			viewBox='0 0 20 20'
-			className='filter_svg__ee-svg'
-			fill='currentColor'
-			height='1.25em'
-			width='1.25em'
-			{...props}
-		>
+		<svg width='1.25em' height='1.25em' viewBox='0 0 20 20' {...props}>
 			<path d='M3 4.5v-2s3.34-1 7-1 7 1 7 1v2l-5 7.03v6.97s-1.22-.09-2.25-.59S8 16.5 8 16.5v-4.97z' />
 		</svg>
 	);
