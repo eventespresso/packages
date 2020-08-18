8820f2b4ce64c7a26cf4a1af599302820841d91a
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-08-18 16:05:22 +0300

    Added useCallback in StyleButton.

--- packages/rich-text-editor/src/components/StyleButton.tsx
+++ packages/rich-text-editor/src/components/StyleButton.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import classNames from 'classnames';
 
 import type { StyleButtonProps } from './types';
 
-const StyleButton: React.FC<StyleButtonProps> = ({ active, style, ...props }) => {
+const StyleButton: React.FC<StyleButtonProps> = ({ active, style, onToggle, ...props }) => {
 	const className = classNames('RichEditor-styleButton', active && 'RichEditor-activeButton');
 
-	const onToggle = (e) => {
-		e.preventDefault();
-		props.onToggle(style);
-	};
+	const onMouseDown = useCallback(
+		(e) => {
+			e.preventDefault();
+			onToggle(style);
+		},
+		[style, onToggle]
+	);
 
 	return (
-		<span className={className} onMouseDown={onToggle} role='button' tabIndex={0}>
+		<span className={className} onMouseDown={onMouseDown} role='button' tabIndex={0}>
 			{props.label}
 		</span>
 	);
