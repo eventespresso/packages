6dd0b5db01909bc4078c4d26a18c9e5223d2e63f
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-08-20 13:01:30 +0300

    Added feature flags feature.

--- packages/services/src/bulkEdit/withBulkEdit.tsx
+++ packages/services/src/bulkEdit/withBulkEdit.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
+import { checkFeatureFlag } from '@eventespresso/config';
+
 import { BulkEditProvider } from './BulkEditProvider';
 import type { AnyObject } from '../';
 
+const isBulkEditEnabled = checkFeatureFlag('bulkEdit');
+
 export const withBulkEdit = <P extends AnyObject>(Component: React.ComponentType<P>): React.FC<P> => {
 	const WrappedComponent: React.FC<P> = (props) => {
-		return (
+		return isBulkEditEnabled ? (
 			<BulkEditProvider>
 				<Component {...props} />
 			</BulkEditProvider>
+		) : (
+			<Component {...props} />
 		);
 	};
 
