056af496bbc2e9cddc6ea1b7e7786202c2cb8d7e
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-08-27 01:03:12 +0530

    memoize onClick

--- domains/rem/src/ui/RemButton.tsx
+++ domains/rem/src/ui/RemButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { __ } from '@wordpress/i18n';
 
 import { Button, NewEntityOption } from '@eventespresso/components';
@@ -8,6 +8,12 @@ import { Rem } from '@eventespresso/icons';
 const RemButton: React.FC = () => {
 	const { open: openRemModal } = useGlobalModal('rem');
 	const { close: closeOptionsPopover } = useGlobalModal('newDatePopover');
+
+	const onClick = useCallback(() => {
+		closeOptionsPopover();
+		openRemModal();
+	}, [closeOptionsPopover, openRemModal]);
+
 	return (
 		<NewEntityOption
 			className={'ee-new-entity-option__recurring-datetime'}
@@ -15,13 +21,7 @@ const RemButton: React.FC = () => {
 			icon={Rem}
 			title={__('Recurring Dates')}
 		>
-			<Button
-				buttonType='primary'
-				onClick={() => {
-					closeOptionsPopover();
-					openRemModal();
-				}}
-			>
+			<Button buttonType='primary' onClick={onClick}>
 				{__('Add Recurring Dates')}
 			</Button>
 		</NewEntityOption>
