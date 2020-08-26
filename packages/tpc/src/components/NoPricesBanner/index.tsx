7cdf66bc94d433f1ef3f412ea794c147ffacac5f
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-08-26 12:59:31 +0300

    Styles applied for Link.

--- packages/tpc/src/components/NoPricesBanner/index.tsx
+++ packages/tpc/src/components/NoPricesBanner/index.tsx
@@ -11,24 +11,21 @@ import type { TicketPriceCalculatorProps } from '../TicketPriceCalculator';
 interface Props extends Pick<TicketPriceCalculatorProps, 'context'> {}
 
 const NoPricesBanner: React.FC<Props> = ({ context }) => {
-	let description =
-		__('Click the button below to load your ') +
-		<DefaultPricesLink>{__('default prices')}</DefaultPricesLink> +
-		__(' into the calculator.') +
-		'\n' +
-		__('Additional ticket price modifiers can be added or removed.');
-
-	if (context === 'editTicketForm') {
-		description +=
-			'\n' +
-			__('Click the save button below to assign which dates this ticket will be available for purchase on.');
-	}
-
 	const title = __('This Ticket is Currently Free');
 
 	return (
 		<Banner status='info' title={title}>
-			{description}
+			<p>
+				{__('Click the button below to load your ')}
+				<DefaultPricesLink>{__('default prices')}</DefaultPricesLink>
+				{__(' into the calculator.')}
+			</p>
+			<p>{__('Additional ticket price modifiers can be added or removed.')}</p>
+			{context === 'editTicketForm' &&
+				'\n' +
+					__(
+						'Click the save button below to assign which dates this ticket will be available for purchase on.'
+					)}
 			<AddDefaultPricesButton />
 		</Banner>
 	);
