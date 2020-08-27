f5f0b3ab74af612bbf88ac6a79ea96a4f004ff6c
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-08-27 18:59:36 +0300

    Oops.

--- packages/form/src/adapters/DatePicker.tsx
+++ packages/form/src/adapters/DatePicker.tsx
@@ -13,7 +13,6 @@ const DatePicker: React.FC<FieldRendererProps> = ({ className, input: { onChange
 
 	return (
 		<div className={htmlClass}>
-			sasas
 			<DatepickerAdapter {...input} dateFormat={dateFormat} locale={locale} {...props} onChange={onChange} />
 		</div>
 	);
