b5c845b199d2f3f882d3e50842891d132925bc7d
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-08-31 19:34:39 +0530

    Memoise useConfigData

--- packages/services/src/config/useConfigData.ts
+++ packages/services/src/config/useConfigData.ts
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import {
 	Currency,
 	CurrencyProps,
@@ -14,34 +15,36 @@ import {
 } from './';
 import { ApiDomData, ConfigDomData } from '../types';
 
-export const useConfigData = (): ConfigDataProps => {
-	const api: ApiDomData = window?.eventEspressoData?.api;
-	const config: ConfigDomData = window?.eventEspressoData?.config;
+const api: ApiDomData = window?.eventEspressoData?.api;
+const config: ConfigDomData = window?.eventEspressoData?.config;
 
-	const configData = {
-		brandName: config?.coreDomain?.brandName || 'Event Espresso',
-		currency: Currency(config?.siteCurrency as CurrencyProps),
-		currentUser: config?.currentUser as CurrentUserProps,
-		generalSettings: config?.generalSettings,
-		dateTimeFormats: DateTimeFormats({
-			dateFormat: config?.generalSettings?.dateFormat,
-			timeFormat: config?.generalSettings?.timeFormat,
-		} as DateTimeFormatsProps),
-		locale: Locale({
-			site: config?.locale?.site || '',
-			siteTimezone: config?.locale?.siteTimezone || {},
-			user: config?.locale?.user || '',
-		} as LocaleProps),
-		nonce: api?.restApiNonce || '',
-		siteUrl: SiteUrl({
-			admin: config?.siteUrls?.admin || '',
-			home: config?.siteUrls?.home || '',
-		} as SiteUrlProps),
-		timezone: Timezone({
-			city: config?.locale?.siteTimezone?.city || '',
-			name: config?.locale?.siteTimezone?.name || '',
-			offset: config?.locale?.siteTimezone?.offset || 0,
-		} as TimezoneProps),
-	};
-	return configData;
+export const useConfigData = (): ConfigDataProps => {
+	return useMemo(
+		() => ({
+			brandName: config?.coreDomain?.brandName || 'Event Espresso',
+			currency: Currency(config?.siteCurrency as CurrencyProps),
+			currentUser: config?.currentUser as CurrentUserProps,
+			generalSettings: config?.generalSettings,
+			dateTimeFormats: DateTimeFormats({
+				dateFormat: config?.generalSettings?.dateFormat,
+				timeFormat: config?.generalSettings?.timeFormat,
+			} as DateTimeFormatsProps),
+			locale: Locale({
+				site: config?.locale?.site || '',
+				siteTimezone: config?.locale?.siteTimezone || {},
+				user: config?.locale?.user || '',
+			} as LocaleProps),
+			nonce: api?.restApiNonce || '',
+			siteUrl: SiteUrl({
+				admin: config?.siteUrls?.admin || '',
+				home: config?.siteUrls?.home || '',
+			} as SiteUrlProps),
+			timezone: Timezone({
+				city: config?.locale?.siteTimezone?.city || '',
+				name: config?.locale?.siteTimezone?.name || '',
+				offset: config?.locale?.siteTimezone?.offset || 0,
+			} as TimezoneProps),
+		}),
+		[]
+	);
 };
