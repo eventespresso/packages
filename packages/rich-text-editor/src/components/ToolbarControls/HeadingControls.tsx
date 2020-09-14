4997f71ba019239cf4468607767560e1ae0a6361
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-09-14 20:31:34 +0300

    Used useMemo.

--- packages/rich-text-editor/src/components/ToolbarControls/HeadingControls.tsx
+++ packages/rich-text-editor/src/components/ToolbarControls/HeadingControls.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Select } from '@eventespresso/adapters';
 import { useMemoStringify } from '@eventespresso/hooks';
@@ -6,8 +6,11 @@ import { HEADING_BLOCK_TYPES } from '../constants';
 import type { BlockStyleControlsProps } from '../types';
 
 const HeadingControls: React.FC<BlockStyleControlsProps> = ({ editorState, onToggle }) => {
-	const selection = editorState.getSelection();
-	const blockType = editorState.getCurrentContent().getBlockForKey(selection.getStartKey()).getType();
+	const selection = useMemo(() => editorState.getSelection(), [editorState]);
+	const blockType = useMemo(() => editorState.getCurrentContent().getBlockForKey(selection.getStartKey()).getType(), [
+		editorState,
+		selection,
+	]);
 	const rootProps = useMemoStringify({ className: 'rich-text-editor-controls__heading' });
 
 	return (
