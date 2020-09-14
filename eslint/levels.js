4c9b06c81a5be58756546465be960f47fa202ec8
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-09-14 15:18:30 +0530

    Add new package validation for ESLint rules

---
+++ eslint/levels.js
@@ -0,0 +1,7 @@
+module.exports = [
+	['constants', 'hooks', 'icons', 'registry', 'rich-text-editor', 'styles', 'toaster', 'dates', 'utils'],
+	['adapters', 'data', 'rrule-generator', 'config'],
+	['form', 'predicates', 'services'],
+	['helpers', 'components', 'edtr-services'],
+	['tpc'],
+];
