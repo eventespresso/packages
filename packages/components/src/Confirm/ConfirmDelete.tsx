95189389d68ea2d5682fa2d801abeadee3ec7f9c
Brent Christensen <brent@eventespresso.com>
2020-09-17 15:47:25 -0700

    oops... don't forget this one

--- packages/components/src/Confirm/ConfirmDelete.tsx
+++ packages/components/src/Confirm/ConfirmDelete.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { __ } from '@eventespresso/i18n';
 
-import type { ConfirmPropsWithButton } from './types';
+import { ModalCloseButtonProps } from '@eventespresso/adapters';
 import useConfirmWithButton from './useConfirmWithButton';
 
-const ConfirmDelete: React.FC<ConfirmPropsWithButton> = (props) => {
+const ConfirmDelete: React.FC<ModalCloseButtonProps> = (props) => {
 	const title = props.title || __('Are you sure you want to delete this?');
 	const confirm = useConfirmWithButton({ ...props, title });
 
