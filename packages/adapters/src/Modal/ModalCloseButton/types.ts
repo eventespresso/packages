744ca0dfe50e2e8660a7eca78b73b5dcac11295e
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-09-18 22:45:03 +0530

    Revert "employ fancy TS Foo"
    This reverts commit 95e71eca26fc3a30d0e8167084c2029f40c02f29.

--- packages/adapters/src/Modal/ModalCloseButton/types.ts
+++ packages/adapters/src/Modal/ModalCloseButton/types.ts
@@ -1,9 +1,6 @@
 import type { CloseButtonProps as ChakraCloseButtonProps } from '@chakra-ui/core';
-import { ButtonProps } from '../../Button/types';
 
-export interface ModalCloseButtonProps
-	extends Partial<Omit<ButtonProps, 'size' | 'type'>>,
-		Partial<ChakraCloseButtonProps> {
+export interface ModalCloseButtonProps extends Partial<ChakraCloseButtonProps> {
 	icon?: React.ComponentType<any>;
 	onConfirm?: VoidFunction;
 	tooltip?: string;
