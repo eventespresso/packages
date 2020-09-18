b703b378982c7bc7dce801b682cf5894292525c2
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-09-18 20:49:33 +0530

    Update OnThe.tsx

--- packages/rrule-generator/src/components/Repeat/Monthly/OnThe.tsx
+++ packages/rrule-generator/src/components/Repeat/Monthly/OnThe.tsx
@@ -62,7 +62,7 @@ const OnThe: React.FC<OnProps> = ({ id, isTheOnlyMode, onChangeMode }) => {
 
 			<select
 				id={`${id}-day`}
-				name='repeat.monthly.onThe.day'
+				name={`${id}-day`}
 				aria-label={__('Repeat monthly on the day')}
 				className='rrule-generator__form-control rrule-generator__select rrule-generator__month'
 				value={onThe.day}
