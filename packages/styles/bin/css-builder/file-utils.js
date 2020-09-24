6341e74ac6d435fa3484d20f0c28b1cbc820ee07
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-09-24 13:42:38 +0300

    Small improvement.

--- packages/styles/bin/css-builder/file-utils.js
+++ packages/styles/bin/css-builder/file-utils.js
@@ -1,7 +1,7 @@
-const { compile } = require( 'handlebars' );
-const fs = require( 'fs' );
-const path = require( 'path' );
-const chalk = require( 'chalk' );
+const { compile } = require('handlebars');
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
 
 /**
  * converts backslashes to slashes
@@ -9,8 +9,8 @@ const chalk = require( 'chalk' );
  * @param {string} path
  * @return {string} path with backslashes converted to slashes
  */
-function fixSlashes( path ) {
-	return path.replace( /\\/g, '/' );
+function fixSlashes(path) {
+	return path.replace(/\\/g, '/');
 }
 
 /**
@@ -19,9 +19,9 @@ function fixSlashes( path ) {
  * @param {Array} pathParts
  * @return {string} rendered template
  */
-function readFile( pathParts ) {
-	const filePath = path.resolve.apply( null, pathParts );
-	return fs.readFileSync( filePath, 'utf8' );
+function readFile(pathParts) {
+	const filePath = path.resolve.apply(null, pathParts);
+	return fs.readFileSync(filePath, 'utf8');
 }
 
 /**
@@ -30,8 +30,8 @@ function readFile( pathParts ) {
  * @param {Array} templatePath
  * @return {Function} HandlebarsTemplateDelegate
  */
-function getTemplateCompiler( templatePath ) {
-	return compile( readFile( templatePath ) );
+function getTemplateCompiler(templatePath) {
+	return compile(readFile(templatePath));
 }
 
 /**
@@ -41,9 +41,9 @@ function getTemplateCompiler( templatePath ) {
  * @param {Object} templateVars
  * @return {string} rendered template
  */
-function parseCssTemplate( templatePath, templateVars ) {
-	const templateCompiler = getTemplateCompiler( templatePath );
-	return templateCompiler( templateVars );
+function parseCssTemplate(templatePath, templateVars) {
+	const templateCompiler = getTemplateCompiler(templatePath);
+	return templateCompiler(templateVars);
 }
 
 /**
@@ -54,15 +54,11 @@ function parseCssTemplate( templatePath, templateVars ) {
  * @param {string} template
  * @param {string} output
  */
-function writeFile( pathParts, fileData, template = '', output = '' ) {
-	const filePath = typeof pathParts === 'object' ?
-		path.resolve.apply( null, pathParts ) :
-		pathParts;
-	fs.writeFileSync( filePath, fileData );
-	if ( template !== '' && output !== '' ) {
-		process.stdout.write(
-			chalk.green( '  \u2022 ' + template + ' \u21D2 ' + output + '\n' )
-		);
+function writeFile(pathParts, fileData, template = '', output = '') {
+	const filePath = typeof pathParts === 'object' ? path.resolve.apply(null, pathParts) : pathParts;
+	fs.writeFileSync(filePath, fileData);
+	if (template !== '' && output !== '') {
+		process.stdout.write(chalk.green('  \u2022 ' + template + ' \u21D2 ' + output + '\n'));
 	}
 }
 
