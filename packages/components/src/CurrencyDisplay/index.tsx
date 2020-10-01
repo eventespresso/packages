4353eb7ef5c27a5d242d4b3e04cc442279e6cb01
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-10-01 21:07:11 +0300

    Moved some components around and limited useMoneyDisplay usage.

--- packages/components/src/CurrencyDisplay/index.tsx
+++ packages/components/src/CurrencyDisplay/index.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import classNames from 'classnames';
 
-import { useConfig } from '@eventespresso/services';
-import { CurrencySign, getCurrencySignCharacterCountClassName } from '../';
+import { useConfig, useMoneyDisplay } from '@eventespresso/services';
+import { getCurrencySignCharacterCountClassName, getCurrencySignPositionClassName } from '@eventespresso/utils';
+import { CurrencySign } from '../';
 import type { CurrencyDisplayProps } from './types';
 
 import './style.scss';
 
 export const CurrencyDisplay: React.FC<CurrencyDisplayProps> = ({ children, value, ...props }) => {
 	const config = useConfig();
+	const { formatAmount } = useMoneyDisplay();
+
 	const currency = config?.currency;
 	const sign = currency?.sign;
 	const signB4 = currency?.signB4;
 
 	const characters = getCurrencySignCharacterCountClassName(sign);
+	const position = getCurrencySignPositionClassName(signB4);
 
-	const className = classNames(
-		props.className,
-		characters,
-		`ee-currency-display-sign--${signB4 ? 'before' : 'after'}`,
-		'ee-currency-display'
-	);
+	const className = classNames(props.className, characters, position, 'ee-currency-display');
 
 	return (
 		<div className={className}>
 			{signB4 && <CurrencySign sign={sign} />}
-			<span>{value}</span>
+			<span>{formatAmount(value)}</span>
 			{!signB4 && <CurrencySign sign={sign} />}
 		</div>
 	);
