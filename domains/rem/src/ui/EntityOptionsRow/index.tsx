322a84d992d76b4d7121f116d9b59c071a7200cb
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-10-01 18:30:30 +0300

    Small improvement.

--- domains/rem/src/ui/EntityOptionsRow/index.tsx
+++ domains/rem/src/ui/EntityOptionsRow/index.tsx
@@ -22,37 +22,35 @@ const EntityOptionsRow: React.FC<EntityOptionsRowProps> = ({
 }) => {
 	const entityType = type === 'datetime' ? __('datetime') : __('ticket');
 	const addNewID = `ee-add-new-${type}`;
-	const describedbyID = `${addNewID}-description`;
+	const addNewDescribedByID = `${addNewID}-description`;
 
 	return (
 		<div className='ee-entity-option__wrapper'>
 			<div className='ee-entity-option__options'>
 				<div className='ee-entity-option__option'>
-					<label className={'ee-focus-priority-5'} htmlFor={'ee-select-' + selectExistingID}>
+					<label className={'ee-focus-priority-5'} htmlFor={`ee-select-${selectExistingID}`}>
 						{
-							/* translators: select an existing "date / ticket" to use as a template */
-							sprintf(__('select an existing %s to use as a template.'), entityType)
+							/* translators: Select an existing "date / ticket" to use as a template */
+							sprintf(__('Select an existing %s to use as a template.'), entityType)
 						}
 					</label>
 					<div className='ee-entity-option__input'>{selectExisting}</div>
 				</div>
 				<div className='ee-entity-option__separator'>{__('or')}</div>
 				<div className='ee-entity-option__option'>
-					<label className={'ee-focus-priority-5'} id={describedbyID}>
+					<label className={'ee-focus-priority-5'} id={addNewDescribedByID}>
 						{
 							/* translators: Add new "date / ticket" and insert details manually */
 							sprintf(__('Add new %s and insert details manually'), entityType)
 						}
 					</label>
-					<div className='ee-entity-option__input'>
-						<Button
-							aria-describedby={describedbyID}
-							buttonText={__('Add New')}
-							id={addNewID}
-							icon={Plus}
-							onClick={onAddNew}
-						/>
-					</div>
+					<Button
+						aria-describedby={addNewDescribedByID}
+						buttonText={__('Add New')}
+						icon={Plus}
+						id={addNewID}
+						onClick={onAddNew}
+					/>
 				</div>
 			</div>
 			{afterOptions}
