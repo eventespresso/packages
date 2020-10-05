9c4646f365da33dcd54e42390b1b53ee9b00d9f3
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-10-05 16:02:13 +0300

    a11y - added missing aria label for modal close button.

--- packages/adapters/src/Modal/modalCloseButtonProps/index.ts
+++ packages/adapters/src/Modal/modalCloseButtonProps/index.ts
@@ -1,9 +1,11 @@
-import type { ButtonProps } from '../../Button';
+import { __ } from '@eventespresso/i18n';
 import { Close } from '@eventespresso/icons';
+import type { ButtonProps } from '../../Button';
 
 import './styles.scss';
 
 const modalCloseButtonProps: ButtonProps = {
+	'aria-label': __('close modal'),
 	className: 'ee-confirm-close ee-icon-button ee-icon-button--borderless',
 	icon: Close,
 };
