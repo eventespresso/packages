fa3a3f98e60d8e706cc7460ae08a461b5089525f
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-10-06 20:15:12 +0300

    Tried to pass children to Switch.

--- domains/eventEditor/src/ui/ticketAssignmentsManager/filters/controls/ShowTrashedTicketsControl.tsx
+++ domains/eventEditor/src/ui/ticketAssignmentsManager/filters/controls/ShowTrashedTicketsControl.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { __ } from '@eventespresso/i18n';
+import { Switch } from '@chakra-ui/core';
 
 import { SwitchInput } from '@eventespresso/components';
 import type { FilterStateManager } from '../filterState';
@@ -8,6 +9,12 @@ type ShowTrashedTicketsControlProps = Pick<FilterStateManager, 'showTrashedTicke
 
 const ShowTrashedTicketsControl: React.FC<ShowTrashedTicketsControlProps> = React.memo(
 	({ showTrashedTickets, setShowTrashedTickets }) => {
+		return (
+			<>
+				<Switch>{__('show trashed tickets')}</Switch>
+			</>
+		);
+
 		return (
 			<SwitchInput
 				label={__('show trashed tickets')}
