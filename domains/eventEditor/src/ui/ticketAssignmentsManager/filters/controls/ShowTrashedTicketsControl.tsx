99bfba8726779c1a4f44aebb8de46a7c420f409b
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-10-06 20:42:28 +0300

    Cleanup

--- domains/eventEditor/src/ui/ticketAssignmentsManager/filters/controls/ShowTrashedTicketsControl.tsx
+++ domains/eventEditor/src/ui/ticketAssignmentsManager/filters/controls/ShowTrashedTicketsControl.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { __ } from '@eventespresso/i18n';
-import { Switch } from '@chakra-ui/core';
 
 import { SwitchInput } from '@eventespresso/components';
 import type { FilterStateManager } from '../filterState';
@@ -9,15 +8,6 @@ type ShowTrashedTicketsControlProps = Pick<FilterStateManager, 'showTrashedTicke
 
 const ShowTrashedTicketsControl: React.FC<ShowTrashedTicketsControlProps> = React.memo(
 	({ showTrashedTickets, setShowTrashedTickets }) => {
-		return (
-			<>
-				<Switch onChange={setShowTrashedTickets}>{__('show trashed tickets')}</Switch>
-				<Switch aria-label={__('show trashed tickets')} onChange={setShowTrashedTickets} />
-				<Switch color='red' />
-				<Switch color='teal' size='lg' />
-			</>
-		);
-
 		return (
 			<SwitchInput
 				label={__('show trashed tickets')}
