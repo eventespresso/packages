30686cc50a78dd6aa52fc8acf5eecff925527f62
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-10-09 21:12:08 +0530

    remove React.memo and improve perf in domains

--- domains/eventEditor/src/ui/tickets/TicketRegistrationsLink.tsx
+++ domains/eventEditor/src/ui/tickets/TicketRegistrationsLink.tsx
@@ -1,34 +1,40 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { addQueryArgs } from '@wordpress/url';
 import { __ } from '@eventespresso/i18n';
 
 import { ADMIN_ROUTES } from '@eventespresso/constants';
 import { getAdminUrl, useEventId } from '@eventespresso/edtr-services';
-import { getPropsAreEqual } from '@eventespresso/utils';
 import { useConfig } from '@eventespresso/services';
-import { useMemoStringify } from '@eventespresso/hooks';
 import { RegistrationsLink, ItemCount } from '@eventespresso/components';
 import { Ticket } from '@eventespresso/edtr-services';
-import type { TooltipProps } from '@eventespresso/adapters';
 
 interface Props {
 	ticket: Ticket;
 }
 
+const tooltipProps = { placement: 'top' as const };
+
 const TicketRegistrationsLink: React.FC<Props> = ({ ticket }) => {
-	const {
-		siteUrl: { admin },
-	} = useConfig();
-	const adminUrl = getAdminUrl({ adminSiteUrl: admin, page: ADMIN_ROUTES.REGISTRATIONS });
+	const { siteUrl } = useConfig();
+
+	const adminUrl = useMemo(() => {
+		return getAdminUrl({ adminSiteUrl: siteUrl.admin, page: ADMIN_ROUTES.REGISTRATIONS });
+	}, [siteUrl.admin]);
+
 	const eventId = useEventId();
-	const regListUrl = addQueryArgs(adminUrl, {
-		event_id: eventId,
-		ticket_id: ticket.dbId,
-		return: 'edit',
-	});
+
+	const args = useMemo(
+		() => ({
+			event_id: eventId,
+			ticket_id: ticket.dbId,
+			return: 'edit',
+		}),
+		[eventId, ticket.dbId]
+	);
+	const regListUrl = addQueryArgs(adminUrl, args);
+
 	const countTitle = __('total registrations.');
 	const tooltip = __('view ALL registrations for this ticket.');
-	const tooltipProps = useMemoStringify<TooltipProps>({ placement: 'top' });
 
 	return (
 		<ItemCount count={ticket.registrationCount} title={countTitle} emphasizeZero={false}>
@@ -37,4 +43,4 @@ const TicketRegistrationsLink: React.FC<Props> = ({ ticket }) => {
 	);
 };
 
-export default React.memo(TicketRegistrationsLink, getPropsAreEqual(['ticket', 'cacheId']));
+export default TicketRegistrationsLink;
