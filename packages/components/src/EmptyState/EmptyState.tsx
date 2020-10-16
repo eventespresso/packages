bcde0bbbc2ccac9b4f4455b83c467a8b8b791c19
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-10-16 20:10:01 +0300

    Small improvement.

--- packages/components/src/EmptyState/EmptyState.tsx
+++ packages/components/src/EmptyState/EmptyState.tsx
@@ -6,7 +6,7 @@ import type { EmptyStateProps } from './types';
 
 import './style.scss';
 
-const iconProps = { name: 'question-outline', size: '96px' };
+const iconProps = { name: 'question-outline', size: '72px' };
 
 const EmptyState: React.FC<EmptyStateProps> = ({ children, description, title, ...props }) => {
 	const className = classNames('ee-empty-state', props.className);
