b8250029373f700a92b53df16e5af0180e254c50
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-10-21 16:05:23 +0530

    Enhance components and disable inline price update for sold tickets

--- domains/eventEditor/src/ui/tickets/ticketsList/editable/EditablePrice.tsx
+++ domains/eventEditor/src/ui/tickets/ticketsList/editable/EditablePrice.tsx
@@ -24,15 +24,24 @@ const EditablePrice: React.FC<Partial<EditablePriceProps>> = ({ entity: ticket, 
 
 	const wrapperProps = useMemoStringify({ className });
 
+	const isEditDisabled = Boolean(ticket.sold);
+
+	const tooltip = isEditDisabled
+		? __(
+				'Ticket price modifications are blocked for Tickets that have already been sold to registrants, because doing so would negatively affect internal accounting for the event. If you still need to modify ticket prices, then create a copy of those tickets, edit the prices for the new tickets, and then archive the old tickets.'
+		  )
+		: __('edit ticket total…');
+
 	return (
 		<InlineEditCurrency
 			id={ticket.id}
 			amount={ticket.price}
+			isEditDisabled={isEditDisabled}
 			placeholder={__('set price…')}
 			wrapperProps={wrapperProps}
 			onChange={onChangePrice}
 			tag={'h3'}
-			tooltip={__('edit ticket total…')}
+			tooltip={tooltip}
 		/>
 	);
 };
