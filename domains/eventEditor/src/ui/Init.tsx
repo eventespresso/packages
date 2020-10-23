5cac348d1d41c03a87548747caeca19c23d63e35
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-10-23 18:45:28 +0530

    Replace entity with etityId in lists

--- domains/eventEditor/src/ui/Init.tsx
+++ domains/eventEditor/src/ui/Init.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { initToaster } from '@eventespresso/toaster';
 import { useEditorInitialization } from '../hooks';
-
-initToaster();
 
 const Init: React.FC = () => {
 	useEditorInitialization();
