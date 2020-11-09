90aca38a74b115e2f418a17b69695fa817fe3133
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-11-09 21:27:41 +0200

    Added the missing import into TextInput.

--- packages/components/src/TextInput/TextInput.tsx
+++ packages/components/src/TextInput/TextInput.tsx
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 
 import { TextInput as TextInputAdapter, TextInputProps } from '@eventespresso/adapters';
 
+import './style.scss';
+
 export const TextInput: React.FC<TextInputProps> = (props) => {
 	const className = classNames('ee-text-input', props.className);
 
