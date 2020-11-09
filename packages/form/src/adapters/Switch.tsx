af5b9c83262115936fd8fbd695112a343d552cb4
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-11-09 14:20:06 +0200

    Casted label prop in Switch component.

--- packages/form/src/adapters/Switch.tsx
+++ packages/form/src/adapters/Switch.tsx
@@ -4,8 +4,8 @@ import { Switch } from '../../../components/src/Switch';
 import withoutMetaProp from './withoutMetaProp';
 import type { FieldRendererProps } from '../types';
 
-const SwitchField: React.FC<FieldRendererProps> = ({ input, ...props }) => {
-	return <Switch {...input} checked={Boolean(input.value)} {...props} />;
+const SwitchField: React.FC<FieldRendererProps> = ({ input, label, ...props }) => {
+	return <Switch {...input} checked={Boolean(input.value)} label={label as string} {...props} />;
 };
 
 export default withoutMetaProp(SwitchField);
