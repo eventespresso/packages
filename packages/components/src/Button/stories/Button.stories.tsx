85f4c868e37cee25fbd1cb07f1301c498f899580
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-11-18 23:11:29 +0530

    Minimal Storybook set up

---
+++ packages/components/src/Button/stories/Button.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Button } from '../../';
+import { ButtonProps } from '../types';
+
+export default {
+	title: 'Components/Button',
+	component: Button,
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	},
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+	buttonText: 'Primary',
+	buttonType: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+	buttonText: 'Secondary',
+	buttonType: 'secondary',
+	size: 'small',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+	buttonText: 'Large',
+	size: 'large',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+	buttonText: 'Small',
+	size: 'small',
+};
