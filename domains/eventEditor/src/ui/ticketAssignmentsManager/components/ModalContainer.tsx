23945b3589e2a02301e5002f62704065552e28e1
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-12-02 19:16:04 +0530

    Open the TAM global modal alert if invalid data

--- domains/eventEditor/src/ui/ticketAssignmentsManager/components/ModalContainer.tsx
+++ domains/eventEditor/src/ui/ticketAssignmentsManager/components/ModalContainer.tsx
@@ -1,19 +1,36 @@
 import React, { useMemo, useCallback } from 'react';
 import { sprintf, __ } from '@eventespresso/i18n';
+import { pick } from 'ramda';
 
 import { EdtrGlobalModals } from '@eventespresso/edtr-services';
 import { useGlobalModal } from '@eventespresso/registry';
+import { useRelations } from '@eventespresso/services';
+import { useConfirmationDialog } from '@eventespresso/components';
 
 import TicketAssignmentsManagerModal from './TicketAssignmentsManagerModal';
 import { withContext } from '../context';
 import { useOnSubmitAssignments } from '../data';
 import type { TAMModalProps } from '../context';
 import type { BaseProps } from '../types';
+import { TAM_ENTITIES } from '../constants';
 
 const ModalContainer: React.FC = () => {
-	const { getData, isOpen, close: onClose } = useGlobalModal<BaseProps>(EdtrGlobalModals.TAM);
+	const { getData, isOpen, close: onClose, openWithData } = useGlobalModal<BaseProps>(EdtrGlobalModals.TAM);
+	const { getData: getRelationalData } = useRelations();
 
 	const submitAssignments = useOnSubmitAssignments();
+
+	const reOpenTamModal = useCallback(() => {
+		openWithData({ assignmentType: 'forAll' });
+	}, [openWithData]);
+
+	const { confirmationDialog, onOpen: showAlert } = useConfirmationDialog({
+		message: __(
+			'There seem to be some dates/tickets which have no tickets/dates assigned. Do you want to fix them now?'
+		),
+		title: __('Alert!'),
+		onConfirm: reOpenTamModal,
+	});
 
 	const { assignmentType, entity } = getData();
 
@@ -21,15 +38,15 @@ const ModalContainer: React.FC = () => {
 
 	if (assignmentType === 'forDate') {
 		title = sprintf(
-			/* translators: %d entity id, %s entity name */
-			__('Ticket Assignment Manager for Datetime: %1$d - %2$s'),
+			/* translators: 1 entity id, 2 entity name */
+			__('Ticket Assignment Manager for Datetime: %1$s - %2$s'),
 			String(entity.dbId),
 			entity.name
 		);
 	} else if (assignmentType === 'forTicket') {
 		title = sprintf(
-			/* translators: %d entity id, %s entity name */
-			__('Ticket Assignment Manager for Ticket: %1$d - %2$s'),
+			/* translators: 1 entity id, 2 entity name */
+			__('Ticket Assignment Manager for Ticket: %1$s - %2$s'),
 			String(entity.dbId),
 			entity.name
 		);
@@ -42,23 +59,47 @@ const ModalContainer: React.FC = () => {
 		title,
 	]);
 
+	const hasOrphanEntities = useCallback(() => {
+		// simplify the data for loop
+		const data = Object.entries(pick(TAM_ENTITIES, getRelationalData()));
+		for (const [, entityRelations] of data) {
+			for (const [, relations] of Object.entries(entityRelations)) {
+				const tamRelations = pick(TAM_ENTITIES, relations);
+				// flatten the relations
+				const relatedIds = Object.values(tamRelations).flat();
+				if (!relatedIds.length) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}, [getRelationalData]);
+
 	const onSubmit = useCallback<TAMModalProps['onSubmit']>(
-		(data) => {
+		async (data) => {
 			// close the moal
 			onClose();
 			// submit TAM data
-			submitAssignments(data);
+			await submitAssignments(data);
+			if (hasOrphanEntities()) {
+				showAlert();
+			}
 		},
-		[onClose, submitAssignments]
+		[hasOrphanEntities, onClose, showAlert, submitAssignments]
 	);
 
 	if (!isOpen) {
-		return null;
+		return <>{confirmationDialog}</>;
 	}
 
 	const Component = withContext(TicketAssignmentsManagerModal, contextProps);
 
-	return <Component title={title} onCloseModal={onClose} onSubmit={onSubmit} />;
+	return (
+		<>
+			<Component title={title} onCloseModal={onClose} onSubmit={onSubmit} />
+			{confirmationDialog}
+		</>
+	);
 };
 
 export default ModalContainer;
