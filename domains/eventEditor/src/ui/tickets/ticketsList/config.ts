65bd76a0f77161c5c0dff907bece52b588127872
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-12-08 16:50:00 +0200

    Small improvement.

--- domains/eventEditor/src/ui/tickets/ticketsList/config.ts
+++ domains/eventEditor/src/ui/tickets/ticketsList/config.ts
@@ -4,10 +4,10 @@ import { __ } from '@eventespresso/i18n';
 
 export const legendConfig: LegendConfig<string> = {
 	icons: [
-		{ icon: IconName.EDIT, description: 'Edit Ticket Details' },
-		{ icon: IconName.CALENDAR, description: 'Manage Date Assignments' },
-		{ icon: IconName.CALCULATOR, description: 'Ticket Price Calculator' },
-		{ icon: Trash, description: 'Move Ticket to Trash' },
+		{ icon: IconName.EDIT, description: __('Edit Ticket Details') },
+		{ icon: IconName.CALENDAR, description: __('Manage Date Assignments') },
+		{ icon: IconName.CALCULATOR, description: __('Ticket Price Calculator') },
+		{ icon: Trash, description: __('Move Ticket to Trash') },
 	],
 	swatches: {
 		TKA: __('Trashed'),
