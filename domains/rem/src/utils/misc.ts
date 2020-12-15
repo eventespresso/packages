20fbc8ff52fa5d35089326a1081a839c226eb930
Brent Christensen <brent@eventespresso.com>
2020-12-15 11:30:22 -0800

    simplify switch (think i was guilty for this)

--- domains/rem/src/utils/misc.ts
+++ domains/rem/src/utils/misc.ts
@@ -59,22 +59,16 @@ export const getMaxDatesLimit = (rruleString: string): number => {
 export const getLimitsWarning = (rRule: string): string => {
 	const freq = getRecurrenceFrequency(rRule);
 
-	let warning = '';
-
-	switch (true) {
-		case freq === 'YEARLY':
-			warning = __('The number of Event Dates has been capped at 5 for YEARLY recurrence patterns');
-			break;
-		case freq === 'MONTHLY':
-			warning = __('The number of Event Dates has been capped at 36 for MONTHLY recurrence patterns (3 years)');
-			break;
-		case freq === 'WEEKLY':
-			warning = __('The number of Event Dates has been capped at 52 for WEEKLY recurrence patterns (1 year)');
-			break;
-		case freq === 'DAILY':
-			warning = __('The number of Event Dates has been capped at 92 for DAILY recurrence patterns (~3 months)');
-			break;
+	switch (freq) {
+		case 'YEARLY':
+			return __('The number of Event Dates has been capped at 5 for YEARLY recurrence patterns');
+		case 'MONTHLY':
+			return __('The number of Event Dates has been capped at 36 for MONTHLY recurrence patterns (3 years)');
+		case 'WEEKLY':
+			return __('The number of Event Dates has been capped at 52 for WEEKLY recurrence patterns (1 year)');
+		case 'DAILY':
+			return __('The number of Event Dates has been capped at 92 for DAILY recurrence patterns (~3 months)');
 	}
 
-	return warning;
+	return '';
 };
