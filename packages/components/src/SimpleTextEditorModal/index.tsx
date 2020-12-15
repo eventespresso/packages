54dde2dd61f770cf533eae2648fb224ad0962c4a
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-12-15 20:54:45 +0200

    Small improvement.

--- packages/components/src/SimpleTextEditorModal/index.tsx
+++ packages/components/src/SimpleTextEditorModal/index.tsx
@@ -68,7 +68,7 @@ export const SimpleTextEditorModal: React.FC<SimpleTextEditorModalProps> = ({
 			>
 				<SimpleTextEditor onChange={onChange} defaultValue={props.text} />
 			</ModalWithAlert>
-			<div className='ee-simple-text-editor__preview'>
+			<div className='ee-simple-text-editor__preview' blabla>
 				<TabbableText
 					className={previewClassName}
 					icon={<Edit className={'ee-inline-edit__edit-icon'} />}
