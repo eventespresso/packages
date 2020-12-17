e89ed4db694eecc71b44b4f34161fbde2b4e5f05
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-12-17 18:49:33 +0200

    Type improvements.

--- packages/components/src/EDTR/RegistrationOptionsMetaBox/types.ts
+++ packages/components/src/EDTR/RegistrationOptionsMetaBox/types.ts
@@ -1,19 +1,18 @@
-// import type { InlineEditProps } from '@eventespresso/adapters';
+import type { InlineEditProps } from '@eventespresso/adapters';
+import type { SelectProps, SwitchProps } from '../../';
 
 export interface RegistrationOptionsMetaBoxProps {
 	allowDonations: boolean;
 	altRegPage: string;
 	displayTicketSelector: boolean;
-	eventManagers: any[];
+	eventManagers: { id: string; name: string }[];
 	managerId: string;
 	maxReg: number;
-
-	onAltRegPageChange: any;
-	onDonationsChange: any;
-	onManagerChange: any;
-	onMaxRegChange: any;
-	onPhoneNumberChange: any;
-	onTicketSelectorChange: any;
-
+	onAltRegPageChange: InlineEditProps['onChange'];
+	onDonationsChange: SwitchProps['onChangeValue'];
+	onManagerChange: SelectProps['onChangeValue'];
+	onMaxRegChange: InlineEditProps['onChange'];
+	onPhoneNumberChange: InlineEditProps['onChange'];
+	onTicketSelectorChange: SwitchProps['onChangeValue'];
 	phoneNumber: string;
 }
