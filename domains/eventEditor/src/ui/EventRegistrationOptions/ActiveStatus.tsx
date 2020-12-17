29eef378d0100ac5a6798192cd62aae54c0e3d9c
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-12-17 21:48:13 +0200

    Extracted datetimeStatus.

--- domains/eventEditor/src/ui/EventRegistrationOptions/ActiveStatus.tsx
+++ domains/eventEditor/src/ui/EventRegistrationOptions/ActiveStatus.tsx
@@ -3,18 +3,9 @@ import classNames from 'classnames';
 
 import { __ } from '@eventespresso/i18n';
 import { GridItem, Select } from '@eventespresso/components';
+import { datetimeStatus } from '@eventespresso/constants';
 import { noop, objectToSelectOptions } from '@eventespresso/utils';
 import { datetimeStatusBgColorClassName } from '@eventespresso/helpers';
-
-const status = {
-	isActive: __('Active'),
-	isCancelled: __('Cancelled'),
-	isExpired: __('Expired'),
-	isInactive: __('Inactive'),
-	isPostponed: __('Postponed'),
-	isSoldOut: __('SoldOut'),
-	isUpcoming: __('Upcoming'),
-};
 
 const ActiveStatus: React.FC = () => {
 	const bgColorClassName = datetimeStatusBgColorClassName(null);
@@ -22,9 +13,9 @@ const ActiveStatus: React.FC = () => {
 
 	const id = 'ee-event-registration-active-status';
 
-	const options = useMemo(() => objectToSelectOptions(status), []);
+	const options = useMemo(() => objectToSelectOptions(datetimeStatus), []);
 
-	const input = <Select onChangeValue={noop} options={options} type='inline' value={status.isActive} />;
+	const input = <Select onChangeValue={noop} options={options} type='inline' value={datetimeStatus.isActive} />;
 
 	return <GridItem className={className} id={id} input={input} label={__('Active status')} />;
 };
