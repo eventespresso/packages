943fae8a830c1a525703a0a5b2b5959e31673936
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-12-23 16:02:14 +0200

    Small improvement.

--- domains/rem/src/ui/rRule/RRuleEditor.tsx
+++ domains/rem/src/ui/rRule/RRuleEditor.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import classNames from 'classnames';
 
 import { FormSectionSidebar } from '@eventespresso/form';
+import { Container } from '../../../../../packages/components';
+
 import PatternEditor from './PatternEditor';
 import type { RRuleEditorProps } from './types';
 
 import './style.scss';
 
 const RRuleEditor: React.FC<RRuleEditorProps> = ({ desc, icon, id, onChange, rRuleString, sidebarLabel, type }) => {
-	const wrapperClassName = classNames('rrule-generator-wrapper', type && `rrule-generator-wrapper-${type}`);
+	// const wrapperClassName = classNames('rrule-generator-wrapper', type && `rrule-generator-wrapper-${type}`);
 
 	return (
-		<div className={wrapperClassName}>
+		<Container content={<PatternEditor id={id} onChange={onChange} rRuleString={rRuleString} type={type} />}>
 			{sidebarLabel && <FormSectionSidebar desc={desc} Icon={icon} title={sidebarLabel} />}
-			<PatternEditor id={id} onChange={onChange} rRuleString={rRuleString} type={type} />
-		</div>
+		</Container>
 	);
 };
 
