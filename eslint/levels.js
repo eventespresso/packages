ab5af3d2a903d550c2f41e0c65727a3646ba64bb
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-12-24 18:49:06 +0530

    Update levels.js

--- eslint/levels.js
+++ eslint/levels.js
@@ -3,9 +3,10 @@ module.exports = [
 	/* LEVEL 2 */ ['data', 'dates', 'hooks', 'icons', 'registry', 'styles', 'storage', 'utils'],
 	/* LEVEL 3 */ ['adapters'],
 	/* LEVEL 4 */ ['ui-components'],
-	/* LEVEL 5 */ ['form', 'rrule-generator', 'rich-text-editor'],
-	/* LEVEL 6 */ ['predicates', 'services'],
-	/* LEVEL 7 */ ['components'], // TODO remove this level
-	/* LEVEL 7 */ ['ee-components', 'edtr-services', 'helpers'],
-	/* LEVEL 8 */ ['tpc'],
+	/* LEVEL 5 */ ['rich-text-editor'],
+	/* LEVEL 6 */ ['form', 'rrule-generator'],
+	/* LEVEL 7 */ ['predicates', 'services'],
+	/* LEVEL 8 */ ['components'], // TODO remove this level
+	/* LEVEL 8 */ ['ee-components', 'edtr-services', 'helpers'],
+	/* LEVEL 9 */ ['tpc'],
 ];
