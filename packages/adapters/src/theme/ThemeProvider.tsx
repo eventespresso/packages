3f7a39e33c46e0b9f717a8831ae6c2fc2cde8e14
Vadim Nicolai <nicolai.vadim@gmail.com>
2020-12-24 12:32:39 +0200

    Cleanup

--- packages/adapters/src/theme/ThemeProvider.tsx
+++ packages/adapters/src/theme/ThemeProvider.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 
-// import theme from './theme';
-
 const ThemeProvider: React.FC = ({ children }) => {
 	return <ChakraProvider>{children}</ChakraProvider>;
 };
