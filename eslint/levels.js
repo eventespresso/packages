d0dc5d3e40d97d0f0e6f5daa577467b2ee2efaff
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-12-24 18:42:42 +0530

    Oops!

--- eslint/levels.js
+++ eslint/levels.js
@@ -3,7 +3,7 @@ module.exports = [
 	/* LEVEL 2 */ ['data', 'dates', 'hooks', 'icons', 'registry', 'styles', 'storage', 'utils'],
 	/* LEVEL 3 */ ['adapters'],
 	/* LEVEL 4 */ ['ui-components'],
-	/* LEVEL 5 */ ['form', 'rrule-generator'],
+	/* LEVEL 5 */ ['form', 'rrule-generator', 'rich-text-editor'],
 	/* LEVEL 6 */ ['predicates', 'services'],
 	/* LEVEL 7 */ ['components'], // TODO remove this level
 	/* LEVEL 7 */ ['ee-components', 'edtr-services', 'helpers'],
