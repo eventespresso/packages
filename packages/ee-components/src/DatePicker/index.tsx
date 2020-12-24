7c4d12187174db20fb83f98f96ad78cb77cbb648
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-12-24 19:33:19 +0530

    Create wrappers for Datepicker components

---
+++ packages/ee-components/src/DatePicker/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { DatePicker as DatePickerAdapter, DatePickerProps } from '@eventespresso/dates';
+import { useConfig } from '@eventespresso/services';
+
+export const DatePicker: React.FC<DatePickerProps> = ({ onChange, value, ...props }) => {
+	const {
+		dateTimeFormats: { dateFormat },
+		locale: { user },
+	} = useConfig();
+
+	return (
+		<DatePickerAdapter
+			className='ee-date-picker'
+			dateFormat={dateFormat}
+			locale={user}
+			onChange={onChange}
+			value={value}
+			{...props}
+		/>
+	);
+};
