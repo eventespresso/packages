703674c4e081052692c73dd4a8db98170bb7ced8
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-01-14 19:04:37 +0530

    Add custom color button

--- packages/ui-components/src/ColorPicker/ColorPicker.tsx
+++ packages/ui-components/src/ColorPicker/ColorPicker.tsx
@@ -1,9 +1,11 @@
 import { useCallback, useEffect, useState } from 'react';
 import classNames from 'classnames';
 
+import { __ } from '@eventespresso/i18n';
 import { ColorPicker as ColorPickerAdapter } from '@eventespresso/adapters';
 import { useIfMounted } from '@eventespresso/hooks';
 
+import { Button, ButtonType } from '../Button';
 import { ColorSwatches } from './ColorSwatches';
 import { ColorPickerProps } from './types';
 import { BLACK_COLOR } from './constants';
@@ -14,6 +16,7 @@ export const ColorPicker: React.FC<ColorPickerProps> = ({ color, defaultColor, o
 	const className = classNames('ee-color-picker', props.className);
 
 	const [internalValue, setInternalValue] = useState(defaultColor || BLACK_COLOR);
+	const [showCustomColor, setShowCustomColor] = useState(false);
 
 	const onChangeColor = useCallback<ColorPickerProps['onChange']>(
 		(newValue) => {
@@ -36,10 +39,26 @@ export const ColorPicker: React.FC<ColorPickerProps> = ({ color, defaultColor, o
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [color]);
 
+	const onClickCustomColor = useCallback(() => setShowCustomColor(true), []);
+
 	return (
 		<div className={className}>
 			<ColorSwatches color={internalValue} onChange={onChangeColor} className='ee-color-picker__swatches' />
-			<ColorPickerAdapter className='ee-color-picker__control' color={internalValue} onChange={onChangeColor} />
+
+			{showCustomColor ? (
+				<ColorPickerAdapter
+					className='ee-color-picker__control'
+					color={internalValue}
+					onChange={onChangeColor}
+				/>
+			) : (
+				<Button
+					buttonText={__('Custom color')}
+					buttonType={ButtonType.MINIMAL}
+					onClick={onClickCustomColor}
+					size='small'
+				/>
+			)}
 		</div>
 	);
 };
