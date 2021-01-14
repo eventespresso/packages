bc6fcbff9fad3e049c0d6c5ce2f3800166b1e1f9
Vadim Nicolai <nicolai.vadim@gmail.com>
2021-01-14 16:26:26 +0200

    Datetime picker width fix on browser zoom out.

---
+++ packages/dates/src/index.stories.tsx
@@ -0,0 +1,25 @@
+// import { useCallback, useState } from 'react';
+import { always } from 'ramda';
+import type { Story, Meta } from '@storybook/react/types-6-0';
+
+import { DateRangePicker, DateTimePicker } from './';
+
+// import type { DateRange } from '@eventespresso/dates';
+import type { DateRangePickerProps } from './types';
+
+export default {
+	component: DateRangePicker,
+	title: 'Components/DateRangePicker',
+} as Meta;
+
+type DateTimeRangePickerStory = Story<DateRangePickerProps>;
+
+export const DatePickerWithTime: DateTimeRangePickerStory = () => (
+	<DateTimePicker
+		dateFormat='MMMM d, yyyy h:mm a'
+		inline
+		locale='en-US'
+		onChange={always(null)}
+		value={new Date('Tue Jan 05 2021 20:42:00 GMT+0200 (Eastern European Standard Time)')}
+	/>
+);
