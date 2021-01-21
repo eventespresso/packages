c558d606ec826473116126045f462dd9392aabe5
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-01-21 17:52:12 +0530

    More improvements

--- domains/eventEditor/src/ui/ticketAssignmentsManager/components/TicketAssignmentsManagerModal/index.tsx
+++ domains/eventEditor/src/ui/ticketAssignmentsManager/components/TicketAssignmentsManagerModal/index.tsx
@@ -1,31 +1,33 @@
+import { useCallback } from 'react';
+
 import { __ } from '@eventespresso/i18n';
 
-import { CollapsibleLegend, ModalWithAlert } from '@eventespresso/ui-components';
+import { CollapsibleLegend, ModalWithAlert, ModalWithAlertProps } from '@eventespresso/ui-components';
 import { legendConfig } from './config';
 import TicketAssignmentsManager from '../TicketAssignmentsManager';
-import useCancelButtonProps from './buttons/useCancelButtonProps';
-import useSubmitButtonProps from './buttons/useSubmitButtonProps';
 import { useDataState } from '../../data';
 import type { TAMModalProps } from '../../context/types';
 
 import '../styles.scss';
 
 const TicketAssignmentsManagerModal: React.FC<Partial<TAMModalProps>> = ({ onCloseModal, onSubmit, title }) => {
-	const { hasOrphanEntities, initialDataIsValid, isDirty } = useDataState();
-	const cancelButtonProps = useCancelButtonProps(onCloseModal, !initialDataIsValid);
-	const submitButtonProps = useSubmitButtonProps(onSubmit);
+	const { hasOrphanEntities, isDirty, getData } = useDataState();
 	const hasErrors = hasOrphanEntities();
+
+	const onSubmitHandler: ModalWithAlertProps['onSubmit'] = useCallback(() => {
+		onSubmit(getData());
+	}, [getData, onSubmit]);
 
 	return (
 		<ModalWithAlert
 			bodyClassName='ee-ticket-assignments-manager__body'
-			cancelButtonProps={cancelButtonProps}
 			className='ee-ticket-assignments-manager'
 			isOpen={true}
+			isSubmitDisabled={hasErrors}
 			onClose={onCloseModal}
 			onCancel={onCloseModal}
+			onSubmit={onSubmitHandler}
 			showAlertOnClose={isDirty || hasErrors}
-			submitButtonProps={submitButtonProps}
 			title={title || __('Ticket Assignment Manager')}
 		>
 			<TicketAssignmentsManager />
