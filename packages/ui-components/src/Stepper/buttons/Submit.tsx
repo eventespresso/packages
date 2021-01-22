cbafcfcd627b1127a3c74bbe8d3adee9c5e93ab4
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-01-22 15:50:49 +0530

    Enhance stepper buttons

--- packages/ui-components/src/Stepper/buttons/Submit.tsx
+++ packages/ui-components/src/Stepper/buttons/Submit.tsx
@@ -3,18 +3,10 @@ import { __ } from '@eventespresso/i18n';
 import { Button, ButtonProps, ButtonType } from '../../../';
 import { SaveOutlined } from '@eventespresso/icons';
 
-const Submit: React.FC<ButtonProps> = ({ isDisabled, onClick, ...props }) => {
+const Submit: React.FC<ButtonProps> = (props) => {
 	const buttonText = props.buttonText || __('Submit');
 
-	return (
-		<Button
-			buttonText={buttonText}
-			buttonType={ButtonType.PRIMARY}
-			onClick={onClick}
-			isDisabled={isDisabled}
-			icon={SaveOutlined}
-		/>
-	);
+	return <Button {...props} buttonText={buttonText} buttonType={ButtonType.PRIMARY} icon={SaveOutlined} />;
 };
 
 export default Submit;
