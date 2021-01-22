2704177ec4e4d2f4e199bde24f9df667b35b86ae
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-01-22 16:28:49 +0530

    Improve REM modal submit

--- packages/ui-components/src/Stepper/buttons/Submit.tsx
+++ packages/ui-components/src/Stepper/buttons/Submit.tsx
@@ -1,12 +1,14 @@
 import { __ } from '@eventespresso/i18n';
+import { SaveOutlined } from '@eventespresso/icons';
 
 import { Button, ButtonProps, ButtonType } from '../../../';
-import { SaveOutlined } from '@eventespresso/icons';
 
 const Submit: React.FC<ButtonProps> = (props) => {
 	const buttonText = props.buttonText || __('Submit');
 
-	return <Button {...props} buttonText={buttonText} buttonType={ButtonType.PRIMARY} icon={SaveOutlined} />;
+	return (
+		<Button buttonText={buttonText} buttonType={ButtonType.PRIMARY} icon={SaveOutlined} type='submit' {...props} />
+	);
 };
 
 export default Submit;
