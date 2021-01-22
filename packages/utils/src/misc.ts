40a834b7694d570e90a1eedb6cfab2e8732dbaa5
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-01-22 15:50:32 +0530

    Add wait utility

--- packages/utils/src/misc.ts
+++ packages/utils/src/misc.ts
@@ -4,3 +4,5 @@ export const isEqualJson = (first: any, second: any): boolean => {
 
 	return firstJson === secondJson;
 };
+
+export const wait = (milliseconds = 0) => new Promise((resolve) => setTimeout(resolve, milliseconds));
