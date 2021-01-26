a05f46ce4329bae10df643cbbdceebf1e7850b38
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-01-26 23:38:59 +0530

    Wire up REM tickets UI

--- domains/rem/src/ui/Tickets/multiStep/Content.tsx
+++ domains/rem/src/ui/Tickets/multiStep/Content.tsx
@@ -1,10 +1,26 @@
+import { useCallback } from 'react';
+
 import { FormWithConfig } from '@eventespresso/ee-components';
 
 import useTicketFormConfig from '../useTicketFormConfig';
 import ContentWrapper from './ContentWrapper';
 import type { ContentProps } from './types';
+import { useFormState } from '../../../data';
 
-const Content: React.FC<ContentProps> = ({ entity, onSubmit, onClose }) => {
+const Content: React.FC<ContentProps> = ({ entity, onClose }) => {
+	const { addTicket, updateTicket } = useFormState();
+
+	const onSubmit = useCallback(
+		(values) => {
+			if (entity?.id) {
+				updateTicket(entity?.id, values);
+			} else {
+				addTicket(values);
+			}
+			onClose();
+		},
+		[addTicket, entity?.id, onClose, updateTicket]
+	);
 	const formConfig = useTicketFormConfig(entity, { onSubmit });
 
 	return <FormWithConfig {...formConfig} formWrapper={ContentWrapper as any} onClose={onClose} />;
