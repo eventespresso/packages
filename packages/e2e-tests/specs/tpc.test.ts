2cbad184bda9157e2681d9f4d5a5c9634044f4cb
Vadim Nicolai <nicolai.vadim@gmail.com>
2021-02-15 17:31:51 +0200

    E2E: TPC - calculate total ticket price.

---
+++ packages/e2e-tests/specs/tpc.test.ts
@@ -0,0 +1,51 @@
+/// <reference types="jest-playwright-preset" />
+/// <reference types="expect-playwright" />
+
+import { saveVideo } from 'playwright-video';
+
+import { addNewPriceModifier, addNewTicket, createNewEvent, removeFreeTicket } from '../utils';
+
+beforeAll(async () => {
+	await saveVideo(page, 'artifacts/calculateTicketTotal.mp4');
+
+	const newTicketName = 'one way ticket';
+	const newTicketAmount = '10';
+
+	await createNewEvent({ title: 'calculateTicketTotal: to be deleted' });
+
+	await removeFreeTicket();
+
+	await addNewTicket({ amount: newTicketAmount, name: newTicketName });
+
+	await page.click('[aria-label="ticket price calculator"]');
+});
+
+describe('TPC', () => {
+	describe('calculateTicketTotal', () => {
+		it('should calculate total ticket price for the given amount of Percent Discount', async () => {
+			await addNewPriceModifier({ amount: '10', priceTypeLabel: 'Percent Discount' });
+			expect(await page.$eval('#ticket-price-total', (el: HTMLInputElement) => el?.value)).toBe('8.5');
+		});
+		it('should calculate total ticket price for the given amount of Dollar Discount', async () => {
+			await addNewPriceModifier({ amount: '2', priceTypeLabel: 'Dollar Discount' });
+			expect(await page.$eval('#ticket-price-total', (el: HTMLInputElement) => el?.value)).toBe('-5');
+		});
+		it('should calculate total ticket price for the given amount of Percent Surcharge', async () => {
+			await addNewPriceModifier({ amount: '3', priceTypeLabel: 'Percent Surcharge' });
+			expect(await page.$eval('#ticket-price-total', (el: HTMLInputElement) => el?.value)).toBe('11.5');
+		});
+		it('should calculate total ticket price for the given amount of Dollar Surcharge', async () => {
+			await addNewPriceModifier({ amount: '4', priceTypeLabel: 'Dollar Surcharge' });
+			expect(await page.$eval('#ticket-price-total', (el: HTMLInputElement) => el?.value)).toBe('25');
+		});
+		it('should calculate total ticket price for the given amount of Regional Tax', async () => {
+			await addNewPriceModifier({ amount: '5', priceTypeLabel: 'Regional Tax' });
+
+			expect(await page.$eval('#ticket-price-total', (el: HTMLInputElement) => el?.value)).toBe('11.5');
+		});
+		it('should calculate total ticket price for the given amount of Federal Tax', async () => {
+			await addNewPriceModifier({ amount: '6', priceTypeLabel: 'Federal Tax' });
+			expect(await page.$eval('#ticket-price-total', (el: HTMLInputElement) => el?.value)).toBe('11.5');
+		});
+	});
+});
