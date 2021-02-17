09f1b06627a38edbd8a49d6faf93743a008ef10d
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-02-17 16:20:47 +0530

    Improve permission types

--- packages/services/src/permissions/types.ts
+++ packages/services/src/permissions/types.ts
@@ -1,6 +1,13 @@
 import type { EntityId } from '@eventespresso/data';
 
-export type EntityType = `${'datetime' | `${'default_' | ''}${'ticket' | 'price' | 'price_type'}`}${'s' | ''}`;
+type EntityWithoutDefault = 'ticket' | 'price' | 'price_type';
+type EntityWithDefault = `${'default_'}${EntityWithoutDefault}`;
+
+export type SingularEntityType = 'datetime' | EntityWithoutDefault | EntityWithDefault;
+
+export type PluralEntityType = `${SingularEntityType}s`;
+
+export type EntityType = SingularEntityType | PluralEntityType;
 
 export type CurrentUserCan = <E extends Record<'userId', EntityId>>(
 	capability: 'read' | 'edit' | 'delete' | Capability,
