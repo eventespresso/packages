95d0091e439daa12ce832a6bb0045bf0a3737ebf
Vadim Nicolai <nicolai.vadim@gmail.com>
2021-02-26 18:54:17 +0200

    Small improvement.

--- packages/ui-components/src/Upsell/index.stories.tsx
+++ packages/ui-components/src/Upsell/index.stories.tsx
@@ -2,7 +2,7 @@ import type { Story, Meta } from '@storybook/react/types-6-0';
 import { boolean, select, withKnobs } from '@storybook/addon-knobs';
 
 import { DataProvider } from '@eventespresso/data';
-import { ConfigProvider } from '@eventespresso/services';
+// import { ConfigProvider } from '@eventespresso/services';
 // import { noop } from '@eventespresso/utils';
 
 import { Upsell } from '../Upsell';
@@ -20,22 +20,22 @@ type UpsellStory = Story<UpsellProps>;
 
 const Template: UpsellStory = (args) => (
 	<DataProvider>
-		<ConfigProvider>
-			<Upsell
-				{...args}
-				altCTAText='Learn more'
-				CTAText='Upgrade today'
-				image='https://eventespresso.com/wp-content/uploads/2016/10/ee4-attendee-mover-380x250.jpg'
-				mainText={
-					args.mainText ||
-					"The value the plugin and the add-ons we ended up using has provided is much, much higher than the cost, and the support I've received is first rate. I can't recommend Event Espresso highly enough.' -Adam Tervort"
-				}
-				mainTitle={args.mainTitle || 'Get more features with Everything subscription'}
-				orientation={select('orientation', ['horizontal', 'vertical'], 'horizontal')}
-				subtitle='Upgrade to Everything Support License'
-				withBorder={boolean('withBorder', false)}
-			/>
-		</ConfigProvider>
+		{/* <ConfigProvider> */}
+		<Upsell
+			{...args}
+			altCTAText='Learn more'
+			CTAText='Upgrade today'
+			image='https://eventespresso.com/wp-content/uploads/2016/10/ee4-attendee-mover-380x250.jpg'
+			mainText={
+				args.mainText ||
+				"The value the plugin and the add-ons we ended up using has provided is much, much higher than the cost, and the support I've received is first rate. I can't recommend Event Espresso highly enough.' -Adam Tervort"
+			}
+			mainTitle={args.mainTitle || 'Get more features with Everything subscription'}
+			orientation={select('orientation', ['horizontal', 'vertical'], 'horizontal')}
+			subtitle='Upgrade to Everything Support License'
+			withBorder={boolean('withBorder', false)}
+		/>
+		{/* </ConfigProvider> */}
 	</DataProvider>
 );
 
