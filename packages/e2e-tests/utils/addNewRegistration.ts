18a8393bd3b07c1312677485cecb261eef979b89
Vadim Nicolai <nicolai.vadim@gmail.com>
2021-03-23 16:43:13 +0200

    Small improvement.

--- packages/e2e-tests/utils/addNewRegistration.ts
+++ packages/e2e-tests/utils/addNewRegistration.ts
@@ -2,16 +2,12 @@
 
 // import { pressKeyWithModifier } from '../utils/press-key-with-modifier';
 
-export const addNewRegistration = async () =>
-	// { ticketName }: any = {}
-	{
-		const regURl = await page.$eval('.ee-editor-details-reg-url-link', (el) => el.getAttribute('href'));
+export const addNewRegistration = async () => {
+	const regURl = await page.$eval('.ee-editor-details-reg-url-link', (el) => el.getAttribute('href'));
 
-		console.log(regURl);
+	await page.goto(regURl);
 
-		await page.goto(regURl);
+	const newReqUrl = await page.$eval('#add-new-registration', (el) => el.getAttribute('href'));
 
-		const newReqUrl = await page.$eval('#add-new-registration', (el) => el.getAttribute('href'));
-
-		await page.goto(newReqUrl);
-	};
+	await page.goto(newReqUrl);
+};
