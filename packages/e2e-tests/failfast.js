6e2aa35cf5c23a27a419464f9af685b991a7bac0
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-03-26 21:42:02 +0530

    Run!!

--- packages/e2e-tests/failfast.js
+++ packages/e2e-tests/failfast.js
@@ -1,7 +1,10 @@
 // Taken from https://github.com/facebook/jest/issues/2867#issuecomment-546592968-permalink
 import * as failFast from 'jasmine-fail-fast';
-
-if (JSON.parse(process.env.npm_config_argv).original.includes('--bail')) {
+/**
+ * Fail after the first test in a single test suite fails. This is NOT the same as jest's
+ * --bail option, which works across test suites
+ */
+if (process.env.FAIL_FAST) {
 	// eslint-disable-next-line jest/no-jasmine-globals, no-undef
 	const jasmineEnv = jasmine.getEnv();
 	jasmineEnv.addReporter(failFast.init());
