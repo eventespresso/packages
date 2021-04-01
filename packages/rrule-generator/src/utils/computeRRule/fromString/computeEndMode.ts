4b8662d4d02e06f85042462da90c73740aa6abe5
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-04-01 18:17:51 +0530

    Fix tests

--- packages/rrule-generator/src/utils/computeRRule/fromString/computeEndMode.ts
+++ packages/rrule-generator/src/utils/computeRRule/fromString/computeEndMode.ts
@@ -3,19 +3,19 @@ import { EndMode } from '../../../types';
 import { RRuleConfig } from '../../../types';
 
 const computeEndMode: ComputeRule<EndMode> = (data, rruleObj, config: RRuleConfig) => {
-	if (rruleObj.count || (rruleObj.count === 0 && config?.endModes?.includes('AFTER'))) {
+	if (rruleObj.count || (rruleObj.count === 0 && config.endModes.includes('AFTER'))) {
 		return 'AFTER';
 	}
 
-	if (rruleObj.until && config?.endModes?.includes('ON_DATE')) {
+	if (rruleObj.until && config.endModes.includes('ON_DATE')) {
 		return 'ON_DATE';
 	}
 
-	if (config?.endModes.includes('NEVER')) {
+	if (config.endModes.includes('NEVER')) {
 		return 'NEVER';
 	}
 
-	return config?.endModes[0];
+	return config.endModes[0];
 };
 
 export default computeEndMode;
