a5dcfb862658e69b154459f4c8c0a9b0f26e742e
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-04-03 00:36:21 +0530

    Create/improve e2e utils

---
+++ packages/e2e-tests/utils/removeAllDates.ts
@@ -0,0 +1,16 @@
+/// <reference types="jest-playwright-preset" />
+import { clickButton } from './';
+
+const selector = '.ee-date-main-menu button';
+
+export const removeAllDates = async () => {
+	let button = await page.$(selector);
+
+	while (button) {
+		await button.click();
+		await clickButton('trash datetime');
+		await clickButton('Yes');
+
+		button = await page.$(selector);
+	}
+};
