ddebc151b57b376cdf0c009224ccebce8311b7a9
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-04-05 23:53:12 +0530

    Update EntityListParser.ts

--- packages/e2e-tests/utils/EntityListParser.ts
+++ packages/e2e-tests/utils/EntityListParser.ts
@@ -30,21 +30,21 @@ export class EntityListParser {
 	};
 
 	/**
-	 * Retrieve  the entity list element.
+	 * Retrieve the entity list root selector.
 	 */
 	getRootSelector = (): string => {
 		return `#ee-entity-list-${this.entityType}s`;
 	};
 
 	/**
-	 * Retrieve  the entity list element.
+	 * Retrieve the entity list element.
 	 */
 	getListRoot = async (): ReturnType<Page['$']> => {
 		return await page.$(this.getRootSelector());
 	};
 
 	/**
-	 * Retrieve  the entity filter bar element.
+	 * Retrieve the entity filter bar element.
 	 */
 	getFilterBar = async (): ReturnType<ElementHandle['$']> => {
 		const entityList = await this.getListRoot();
@@ -53,14 +53,14 @@ export class EntityListParser {
 	};
 
 	/**
-	 * Retrieve  the entity items wrapper based on the current view.
+	 * Retrieve the entity items wrapper based on the current view.
 	 */
 	getItemsListWrapper = async (): ReturnType<ElementHandle['$']> => {
 		return this.view === 'card' ? this.getCardList() : this.getTableList();
 	};
 
 	/**
-	 * Retrieve  the entity card view list wrapper element.
+	 * Retrieve the entity card view list wrapper element.
 	 */
 	getCardList = async (): ReturnType<ElementHandle['$']> => {
 		const entityList = await this.getListRoot();
@@ -69,7 +69,7 @@ export class EntityListParser {
 	};
 
 	/**
-	 * Retrieve  the entity table view list wrapper element.
+	 * Retrieve the entity table view list wrapper element.
 	 */
 	getTableList = async (): ReturnType<ElementHandle['$']> => {
 		const entityList = await this.getListRoot();
@@ -123,7 +123,7 @@ export class EntityListParser {
 	};
 
 	/**
-	 * Retrieve  the field value of an item/element.
+	 * Retrieve the field value of an item/element.
 	 */
 	getItemField = async (item: Item, field: Field): Promise<string | number> => {
 		switch (field) {
@@ -138,7 +138,7 @@ export class EntityListParser {
 	};
 
 	/**
-	 * Retrieve  the dbId of an item/element. Default to first item.
+	 * Retrieve the dbId of an item/element. Default to first item.
 	 */
 	getItemDbId = async (item?: Item): Promise<number> => {
 		const targetItem = item || (await this.getItem());
@@ -155,7 +155,7 @@ export class EntityListParser {
 	};
 
 	/**
-	 * Retrieve  the name of an item/element. Default to first item.
+	 * Retrieve the name of an item/element. Default to first item.
 	 */
 	getItemName = async (item?: Item): Promise<string> => {
 		const targetItem = item || (await this.getItem());
@@ -165,7 +165,7 @@ export class EntityListParser {
 	};
 
 	/**
-	 * Retrieve  the description of an item/element. Default to first item.
+	 * Retrieve the description of an item/element. Default to first item.
 	 */
 	getItemDesc = async (item?: Item): Promise<string> => {
 		const targetItem = item || (await this.getItem());
@@ -177,7 +177,7 @@ export class EntityListParser {
 	};
 
 	/**
-	 * Retrieve  the dbIds of all the items in the list.
+	 * Retrieve the dbIds of all the items in the list.
 	 */
 	getDbIds = async (): Promise<Array<number>> => {
 		const items = await this.getListItems();
@@ -197,7 +197,7 @@ export class EntityListParser {
 	};
 
 	/**
-	 * Retrieve  the number of items in the list.
+	 * Retrieve the number of items in the list.
 	 */
 	getItemCount = async (): Promise<number> => {
 		const items = await this.getListItems();
@@ -206,7 +206,7 @@ export class EntityListParser {
 	};
 
 	/**
-	 * Retrieve  the dbId of an item in the list by its name.
+	 * Retrieve the dbId of an item in the list by its name.
 	 */
 	getDbIdByName = async (name: string): Promise<number> => {
 		const item = await this.getItemBy('name', name);
