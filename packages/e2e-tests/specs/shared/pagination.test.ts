4d42d7f57af956ef284fd431e8faf77f4f997f37
Vadim Nicolai <nicolai.vadim@gmail.com>
2021-04-06 15:14:08 +0300

    Small improvement.

--- packages/e2e-tests/specs/shared/pagination.test.ts
+++ packages/e2e-tests/specs/shared/pagination.test.ts
@@ -1,7 +1,7 @@
 import { saveVideo } from 'playwright-video';
 
-import { addNewEntity, createNewEvent } from '../../utils';
-import { getEntitiesLength, getPaginationSize } from '../../assertions';
+import { addNewEntity, createNewEvent, EntityListParser } from '../../utils';
+import { getPaginationSize } from '../../assertions';
 import { entities } from '../../constants';
 
 const namespace = 'event.entities.pagination';
@@ -16,6 +16,7 @@ describe(namespace, () => {
 			'should check if pagination changes according to the logic related to number of entities: ' + entity,
 			async () => {
 				const newName = `new ${entity}`;
+				const parser = new EntityListParser(entity);
 
 				await createNewEvent({ title: `${namespace}.${entity}` });
 
@@ -25,16 +26,16 @@ describe(namespace, () => {
 					await addNewEntity({ entity, name: `${newName} #${index + 1}` });
 				}
 
-				expect(await getEntitiesLength(entity)).toBe(6);
+				expect(await parser.getItemCount()).toBe(6);
 				expect(await getPaginationSize(entity)).toBe(2);
 
 				await page.click(`#ee-entity-list-${entity}s .ee-pagination .rc-pagination-item >> text=2`);
-				expect(await getEntitiesLength(entity)).toBe(2);
+				expect(await parser.getItemCount()).toBe(2);
 
 				await page.selectOption('.ee-pagination__per-page-select-wrapper select', {
 					value: '2',
 				});
-				expect(await getEntitiesLength(entity)).toBe(2);
+				expect(await parser.getItemCount()).toBe(2);
 			}
 		);
 	}
