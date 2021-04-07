67ef5a02eaa2cea4bd893479363541658bb03155
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-04-08 01:30:43 +0530

    use toaster-notice

--- packages/e2e-tests/specs/tam/utils.ts
+++ packages/e2e-tests/specs/tam/utils.ts
@@ -1,12 +1,7 @@
 import { data } from './data';
 import { addNewDate, addNewTicket } from '../../utils';
 
-const datesListSelector = '#ee-entity-list-datetimes .ee-entity-list__card-view';
-
 export const addDatesAndTickets = async () => {
-	// Wait for the list lazy load
-	await page.waitForFunction((selector) => document.querySelector(selector), datesListSelector);
-
 	// add dates
 	for (const item of data) {
 		await addNewDate({ ...item, name: 'Date ' + item.name });
@@ -16,6 +11,4 @@ export const addDatesAndTickets = async () => {
 	for (const item of data) {
 		await addNewTicket({ ...item, name: 'Ticket ' + item.name, amount: 10 });
 	}
-
-	await page.waitForTimeout(1000);
 };
