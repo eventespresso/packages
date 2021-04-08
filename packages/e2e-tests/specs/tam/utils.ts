7f1aad9c0141e64a1e1568c968fd2282a6c2fe7d
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-04-08 15:48:21 +0530

    Revert "use toaster-notice"
    This reverts commit 67ef5a02eaa2cea4bd893479363541658bb03155.

--- packages/e2e-tests/specs/tam/utils.ts
+++ packages/e2e-tests/specs/tam/utils.ts
@@ -1,7 +1,12 @@
 import { data } from './data';
 import { addNewDate, addNewTicket } from '../../utils';
 
+const datesListSelector = '#ee-entity-list-datetimes .ee-entity-list__card-view';
+
 export const addDatesAndTickets = async () => {
+	// Wait for the list lazy load
+	await page.waitForFunction((selector) => document.querySelector(selector), datesListSelector);
+
 	// add dates
 	for (const item of data) {
 		await addNewDate({ ...item, name: 'Date ' + item.name });
@@ -11,4 +16,6 @@ export const addDatesAndTickets = async () => {
 	for (const item of data) {
 		await addNewTicket({ ...item, name: 'Ticket ' + item.name, amount: 10 });
 	}
+
+	await page.waitForTimeout(1000);
 };
--- packages/e2e-tests/utils/addNewTicket.ts
+++ packages/e2e-tests/utils/addNewTicket.ts
@@ -1,6 +1,8 @@
 import { clickButton, setPrice, fillDateTicketForm, DateTicketFormArgs } from './';
+import { EntityListParser } from './EntityListParser';
 
-const toastSelector = `.ee-toaster-notice__toast`;
+const listParser = new EntityListParser('ticket');
+const wrapperSelector = `${listParser.getRootSelector()} .ee-entity-list__card-view`;
 
 export const addNewTicket = async ({ amount, ...fields }: DateTicketFormArgs & { amount?: number }) => {
 	await page.click('text=Add New Ticket');
@@ -24,18 +26,36 @@ export const addNewTicket = async ({ amount, ...fields }: DateTicketFormArgs & {
 
 	await page.click('[aria-label="assign ticket"]');
 
+	const countBeforeSubmit = await listParser.getItemCount();
+
 	await page.click('button[type=submit]');
 
 	// Wait for tickets list to update
-	await page.waitForFunction((selector) => {
-		const toastBody = document.querySelector(`${selector} .ee-toaster-notice__toast-body`).textContent;
+	await page.waitForFunction(
+		([selector, prevCount]) => {
+			// if there is no list wrapper, it means ticket is not created yet
+			if (!document.querySelector(selector)) {
+				return false;
+			}
 
-		return toastBody.includes('successfully created ticket');
-	}, toastSelector);
+			const items = document.querySelectorAll(`${selector} .ee-entity-list-item`);
+			const nextCount = items.length;
 
-	try {
-		await page.click(`${toastSelector} .Toastify__close-button`);
-	} catch (error) {
-		// do nothing
-	}
+			// if number of previous items is same as new, ticket is not in the list yet
+			if (prevCount === nextCount) {
+				return false;
+			}
+
+			// lets loop through each item
+			for (const item of items.values()) {
+				// if the item id includes 'temp:', it means it is the optimistic response from Apollo, lets ignore it
+				const id = item.id;
+				if (id.includes('temp:')) {
+					return false;
+				}
+			}
+			return true;
+		},
+		[wrapperSelector, countBeforeSubmit] as const
+	);
 };
