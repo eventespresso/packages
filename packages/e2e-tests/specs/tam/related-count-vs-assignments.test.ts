91b16996d25971f1ae9b605cedfb7ab1cc5e6b3e
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-04-09 18:18:14 +0530

    Make the test more comprehensive

--- packages/e2e-tests/specs/tam/related-count-vs-assignments.test.ts
+++ packages/e2e-tests/specs/tam/related-count-vs-assignments.test.ts
@@ -1,64 +1,94 @@
 import { saveVideo } from 'playwright-video';
 
-import { clickLabel, createNewEvent, EntityListParser, TAMRover } from '../../utils';
+import { clickLabel, createNewEvent, EntityListParser, TAMRover, GetMapProps, EntityType, ListView } from '../../utils';
 import { addDatesAndTickets } from './utils';
 
 const tamrover = new TAMRover();
 const parser = new EntityListParser('datetime', 'card');
 
 beforeAll(async () => {
-	await saveVideo(page, 'artifacts/tam-related-count-vs-assignments.mp4');
+	// await saveVideo(page, 'artifacts/tam-related-count-vs-assignments.mp4');
 
-	await createNewEvent({ title: 'TAM: Related Count in card and table view vs TAM Assignments' });
-	await addDatesAndTickets();
+	// await createNewEvent({ title: 'TAM: Related Count in card and table view vs TAM Assignments' });
+	// await addDatesAndTickets();
+
+	await page.goto(
+		'http://eventsmart.local.host/wp-admin/admin.php?page=espresso_events&action=edit&post=96&return=default'
+	);
 
 	await page.waitForTimeout(1000);
 });
+
+const toggleAllFilters = async () => {
+	await clickLabel('show trashed dates');
+	await clickLabel('show expired tickets');
+	await clickLabel('show trashed tickets');
+};
+
+const assertListAndTAMRelatedCount = async (entityType: EntityType, viewType: ListView, closeModal = true) => {
+	// Set entity and switch to th view type before getting the ids
+	await parser.setEntityType(entityType).switchView(viewType);
+
+	// create a map of db id and related item count
+	const relatedCountInList = await parser.getRelatedItemsCountMap();
+
+	// Open TAM for all dates/tickets
+	await tamrover.setForType('all').launch();
+
+	// Toggle filters
+	await toggleAllFilters();
+
+	const tamMapOptions: GetMapProps = {
+		forceGenerate: true,
+		mapFromTo: entityType === 'ticket' ? 'ticket2dates' : 'date2tickets',
+	};
+
+	const relatedCountInTAM = await tamrover.getAssigmentsCountMap(tamMapOptions);
+
+	/******** CHECK RELATIONS BEFORE TOGGLE ********/
+	for (const dbId in relatedCountInList) {
+		const countInList = relatedCountInList[dbId];
+		const countInTAM = relatedCountInTAM[dbId];
+
+		expect(countInList).toEqual(countInTAM);
+	}
+
+	// Toggle filters
+	await toggleAllFilters();
+
+	if (closeModal) {
+		// Close TAM modal
+		await tamrover.close();
+	}
+};
 
 describe('TAM:RelatedCountVsAssignments', () => {
 	// check for both dates and tickets
 	for (const entityType of ['datetime', 'ticket'] as const) {
 		// check for both the views
 		for (const viewType of ['card', 'table'] as const) {
+			// eslint-disable-next-line jest/expect-expect
 			it(`tests related items count for ${entityType}s in ${viewType} view vs assignments count in TAM`, async () => {
-				// Set entity and switch to th view type before getting the ids
-				await parser.setEntityType(entityType).switchView(viewType);
+				/******** CHECK BEFORE SUBMIT ********/
+				await assertListAndTAMRelatedCount(entityType, viewType, false);
 
-				const dbIds = await parser.getDbIds();
-				// create a map of db id and related item count
-				const relatedCountInList: Record<number, number> = {};
-				// check for each entity in the list
-				for (const dbId of dbIds) {
-					relatedCountInList[dbId] = await parser.getRelatedItemsCount(dbId);
-				}
+				// Lets flip all the relations
+				await tamrover.toggleAllAssignments();
 
-				// Open TAM for all dates/tickets
-				await tamrover.setForType('all').launch();
+				// Now lets submit.
+				await tamrover.submit();
+				await page.waitForTimeout(15000);
 
-				// Toggle filters
-				await clickLabel('show trashed dates');
-				await clickLabel('show expired tickets');
-				await clickLabel('show trashed tickets');
+				/******** CHECK AFTER SUBMIT ********/
+				await assertListAndTAMRelatedCount(entityType, viewType);
 
-				const mapFromTo = entityType === 'ticket' ? 'ticket2dates' : 'date2tickets';
-				const map = await tamrover.getMap({ forceGenerate: true, mapFromTo });
+				// Reload the page.
+				await page.reload();
 
-				const relatedCountInTAM: Record<number, number> = Object.entries(map).reduce(
-					(prevMap, [entityId, relationMap]) => {
-						return { ...prevMap, [entityId]: Object.values(relationMap).filter((v) => v === 'OLD').length };
-					},
-					{}
-				);
+				await page.waitForSelector('[type=button] >> text=Ticket Assignments');
 
-				for (const dbId in relatedCountInList) {
-					const countInList = relatedCountInList[dbId];
-					const countInTAM = relatedCountInTAM[dbId];
-
-					expect(countInList).toEqual(countInTAM);
-				}
-
-				// Close TAM modal
-				await tamrover.close();
+				/******** CHECK AFTER RELOAD ********/
+				await assertListAndTAMRelatedCount(entityType, viewType);
 			});
 		}
 	}
