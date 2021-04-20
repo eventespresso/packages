c2a15d4380f62c7db8e324abc410d615abaa3100
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-04-20 18:49:58 +0530

    Update ThemeProvider.tsx

--- packages/adapters/src/theme/ThemeProvider.tsx
+++ packages/adapters/src/theme/ThemeProvider.tsx
@@ -4,9 +4,11 @@ import { isRTL } from '@eventespresso/i18n';
 
 const theme = extendTheme({ styles: null, direction: isRTL() ? 'rtl' : 'ltr' });
 
+const environment = { window, document };
+
 const ThemeProvider: React.FC = ({ children }) => {
 	return (
-		<ChakraProvider theme={theme} resetCSS>
+		<ChakraProvider theme={theme} resetCSS environment={environment}>
 			{children}
 		</ChakraProvider>
 	);
