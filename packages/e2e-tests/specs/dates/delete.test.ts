9c492c45a6ca2b41633b7def30e23313dc83cc78
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-04-30 19:25:30 +0530

    Restore workflow

--- packages/e2e-tests/specs/dates/delete.test.ts
+++ packages/e2e-tests/specs/dates/delete.test.ts
@@ -8,37 +8,32 @@ const dateEditor = new DateEditor();
 const ticketsParser = new EntityListParser('ticket');
 
 beforeAll(async () => {
-	// await saveVideo(page, `artifacts/${namespace}.mp4`);
+	await saveVideo(page, `artifacts/${namespace}.mp4`);
 	await createNewEvent({ title: namespace });
 });
 
 describe(namespace, () => {
 	it('should delete dates by name:', async () => {
-		const capture = await saveVideo(page, `artifacts/${namespace}.mp4`);
-		try {
-			await addNewDate({ name: namespace + '.date' });
-			let dateCount = await dateEditor.getItemCount();
+		await addNewDate({ name: namespace + '.date' });
+		let dateCount = await dateEditor.getItemCount();
 
-			expect(dateCount).toBe(2);
+		expect(dateCount).toBe(2);
 
-			await dateEditor.deleteItemBy('name', namespace + '.date');
+		await dateEditor.deleteItemBy('name', namespace + '.date');
 
-			dateCount = await dateEditor.getItemCount();
-			let ticketCount = await ticketsParser.getItemCount();
+		dateCount = await dateEditor.getItemCount();
+		let ticketCount = await ticketsParser.getItemCount();
 
-			expect(dateCount).toBe(1);
-			expect(ticketCount).toBe(1);
+		expect(dateCount).toBe(1);
+		expect(ticketCount).toBe(1);
 
-			const date = await dateEditor.getItem();
+		const date = await dateEditor.getItem();
 
-			await dateEditor.deleteItem(date);
+		await dateEditor.deleteItem(date);
 
-			dateCount = await dateEditor.getItemCount();
-			ticketCount = await ticketsParser.getItemCount();
-			expect(dateCount).toBe(0);
-			expect(ticketCount).toBe(0);
-		} catch (error) {
-			await capture.stop();
-		}
+		dateCount = await dateEditor.getItemCount();
+		ticketCount = await ticketsParser.getItemCount();
+		expect(dateCount).toBe(0);
+		expect(ticketCount).toBe(0);
 	});
 });
