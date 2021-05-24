f925c754fdeced6712a5d9a09d84338536a45056
Brent Christensen <brent@eventespresso.com>
2021-05-24 12:42:11 -0700

    oop! close active element

--- packages/ui-components/src/FormBuilder/useOpenElement.ts
+++ packages/ui-components/src/FormBuilder/useOpenElement.ts
@@ -8,6 +8,13 @@ export const useOpenElement = (): openCloseElement => {
 	const [openElement, setOpenElement] = useState('');
 	const isOpen = (UUID: string) => UUID === openElement;
 	// onClick handler that must be primed (curried) with the form element's UUID
-	const toggleElement = useCallback((UUID: string) => () => setOpenElement(UUID), [setOpenElement]);
+	const toggleElement = useCallback(
+		(UUID: string) => () => {
+			// if passed UUID matches the currently open element, then close that element (by unsetting UUID)
+			const activeElement = UUID !== openElement ? UUID : '';
+			setOpenElement(activeElement);
+		},
+		[openElement, setOpenElement]
+	);
 	return { isOpen, toggleElement };
 };
