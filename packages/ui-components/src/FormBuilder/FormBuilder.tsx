70f81440314d3b5961b65b091d8edc1efcaddf7f
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-05-25 22:40:13 +0530

    Create Form Builder state

--- packages/ui-components/src/FormBuilder/FormBuilder.tsx
+++ packages/ui-components/src/FormBuilder/FormBuilder.tsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 
 import { Container } from '../Container';
 import { FormSection } from './FormSection';
-import { useOpenElement } from './useOpenElement';
+import { withFormState } from './context';
 
 import type { FormBuilderProps } from './types';
 import './styles.scss';
@@ -30,21 +30,11 @@ const FormBuilder: React.FC<FormBuilderProps> = ({
 		};
 	}, [bodyClass, containerClass, contentClass, sidebarClass]);
 
-	// controls and tracks which element is open for editing
-	const { isOpen, toggleElement } = useOpenElement();
-
 	const form = useMemo(() => {
-		return formSections.map((formSection) => (
-			<FormSection
-				key={formSection.UUID}
-				formSection={formSection}
-				isOpen={isOpen}
-				toggleElement={toggleElement}
-			/>
-		));
-	}, [formSections, isOpen, toggleElement]);
+		return formSections.map((formSection) => <FormSection key={formSection.UUID} formSection={formSection} />);
+	}, [formSections]);
 
 	return <Container classes={classes} content={form} header={header} />;
 };
 
-export default FormBuilder;
+export default withFormState(FormBuilder);
