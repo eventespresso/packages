65ef556beae3631c5ed9cecd399d74276d738d60
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-05-26 23:45:55 +0530

    Fix types

--- packages/ui-components/src/FormBuilder/FormElement/FormElementInput.tsx
+++ packages/ui-components/src/FormBuilder/FormElement/FormElementInput.tsx
@@ -1,9 +1,9 @@
 import { FormControl } from '@eventespresso/adapters';
 
 import { MappedElement } from './MappedElement';
-import type { FormInputProps } from '../types';
+import type { FormElementProps } from '../types';
 
-export const FormElementInput: React.FC<FormInputProps> = ({ element }) => {
+export const FormElementInput: React.FC<FormElementProps> = ({ element }) => {
 	return (
 		<FormControl className='ee-form-element__input' isRequired={element.required}>
 			<MappedElement element={element} />
