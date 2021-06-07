a500edd7d996ab8a67f45f2eacd49a6846c3a625
Brent Christensen <brent@eventespresso.com>
2021-06-07 16:21:18 -0700

    remove svgName prop

--- packages/icons/src/withClassName.tsx
+++ packages/icons/src/withClassName.tsx
@@ -4,13 +4,12 @@ import { IconComponent } from './types';
 
 import './style.scss';
 
-const withClassName = (WrappedComponent: IconComponent, svgName = ''): IconComponent => {
+const withClassName = (WrappedComponent: IconComponent): IconComponent => {
 	const WrappedIcon: IconComponent = ({ noMargin, size, ...props }) => {
 		const className = classNames(
 			'ee-svg',
 			size && `ee-icon--${size}`,
 			noMargin && 'ee-icon--no-margin',
-			svgName && `ee-svg--${svgName}`,
 			props.className
 		);
 
