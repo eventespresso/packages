31b4a03e1775542d6bd6f16b20602659236d1c35
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-06-21 21:43:19 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,16 @@
 module.exports = function (api) {
 	api.cache(true);
 
-	const presets = ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'];
+	const presets = [
+		'@babel/preset-env',
+		[
+			'@babel/preset-react',
+			{
+				runtime: 'automatic',
+			},
+		],
+		'@babel/preset-typescript',
+	];
 
 	const plugins = ['@babel/plugin-transform-runtime'];
 
