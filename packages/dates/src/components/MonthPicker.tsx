d4ef309a7cde600d6540116ea7afd3ce99ffb80f
Manzoor Wani <manzoorwani.jk@gmail.com>
2021-07-07 15:48:40 +0530

    Improve MonthPicker

--- packages/dates/src/components/MonthPicker.tsx
+++ packages/dates/src/components/MonthPicker.tsx
@@ -2,5 +2,5 @@ import { DatePicker } from './DatePicker';
 import type { DatePickerProps } from '../types';
 
 export const MonthPicker: React.FC<DatePickerProps> = (props) => {
-	return <DatePicker calendarClassName='ee-month-picker' {...props} />;
+	return <DatePicker calendarClassName='ee-month-picker' dateFormat='MM/yyyy' showMonthYearPicker {...props} />;
 };
